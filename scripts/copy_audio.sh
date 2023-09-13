6aeaf22cdf8e453a38cc927780123fff8c40b65e
Chris Olivier <cjolivier01@apache.org>
2023-09-13 10:46:06 -0700

    ..

--- scripts/copy_audio.sh
+++ scripts/copy_audio.sh
@@ -3,8 +3,7 @@ set -e
 INPUT_VIDEO_WITH_AUDIO="$1"
 INPUT_VIDEO_NO_AUDIO="$2"
 OUTPUT_VIDEO_WITH_AUDIO="$3"
-#ffmpeg -i "$1" -vn -acodec copy output-audio.aac
-#ffmpeg -i "$2" -i output-audio.aac -map 0:v -map 1:a -c:v copy -shortest "$3"
+
 ffmpeg \
   -i "${INPUT_VIDEO_WITH_AUDIO}" \
   -i "${INPUT_VIDEO_NO_AUDIO}" \
