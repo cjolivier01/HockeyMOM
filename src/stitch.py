9dd045c6f24a06e039dcb56aa7f2e3ee109bc275
Chris Olivier <cjolivier01@apache.org>
2023-09-13 03:38:18 -0700

    stitch

--- src/stitch.py
+++ src/stitch.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torch.nn as nn
 
@@ -14,14 +15,15 @@ import time
 
 class Timer(object):
     """A simple timer."""
-    def __init__(self):
-        self.total_time = 0.
-        self.calls = 0
-        self.start_time = 0.
-        self.diff = 0.
-        self.average_time = 0.
 
-        self.duration = 0.
+    def __init__(self):
+        self.total_time = 0.0
+        self.calls = 0
+        self.start_time = 0.0
+        self.diff = 0.0
+        self.average_time = 0.0
+
+        self.duration = 0.0
 
     def tic(self):
         # using time.time instead of time.clock because time time.clock
@@ -40,49 +42,53 @@ class Timer(object):
         return self.duration
 
     def clear(self):
-        self.total_time = 0.
+        self.total_time = 0.0
         self.calls = 0
-        self.start_time = 0.
-        self.diff = 0.
-        self.average_time = 0.
-        self.duration = 0.
+        self.start_time = 0.0
+        self.diff = 0.0
+        self.average_time = 0.0
+        self.duration = 0.0
 
 
 def get_vid_metadata(vid):
     vid_cap = cv2.VideoCapture(vid.as_posix())
-    return vid_cap.get(7), vid_cap.get(5), vid_cap.get(7)/vid_cap.get(5)
+    return vid_cap.get(7), vid_cap.get(5), vid_cap.get(7) / vid_cap.get(5)
+
 
 def create_wide_df(df):
-    df['event'] = df['event'].shift(-1)
-    df['event_attributes'] = df['event_attributes'].shift(-1)
-    df['start_time'] = df['time']
-    df['event_time'] = df['time'].shift(-1)
-    df['end_time'] = df['time'].shift(-2)
+    df["event"] = df["event"].shift(-1)
+    df["event_attributes"] = df["event_attributes"].shift(-1)
+    df["start_time"] = df["time"]
+    df["event_time"] = df["time"].shift(-1)
+    df["end_time"] = df["time"].shift(-2)
     df = df.query('event not in ["start", "end"]')
-    df = df.drop(['time'], axis=1)
+    df = df.drop(["time"], axis=1)
     return df
+
 
 def create_new_interval_vid_df(vid, df, interval=2):
     _, _, length = get_vid_metadata(vid)
 
     start_times = np.arange(0, length, interval)
-    end_times = np.arange(interval-0.04, length+interval-0.04, interval)
+    end_times = np.arange(interval - 0.04, length + interval - 0.04, interval)
 
     if end_times[-1] > length:
         end_times[-1] = length
 
     vid_id = vid.name[:-4]
-    vid_df = pd.DataFrame({
-        'vid_id': vid_id,
-        'start_time': start_times,
-        'end_time': end_times,
-        'event': 'background',
-    })
+    vid_df = pd.DataFrame(
+        {
+            "vid_id": vid_id,
+            "start_time": start_times,
+            "end_time": end_times,
+            "event": "background",
+        }
+    )
 
-    old_vid_df = df.query('video_id == @vid_id')
+    old_vid_df = df.query("video_id == @vid_id")
 
     for event_time, event in zip(old_vid_df.event_time, old_vid_df.event):
-        vid_df.loc[np.argmax(event_time < start_times), 'event'] = event
+        vid_df.loc[np.argmax(event_time < start_times), "event"] = event
 
     return vid_df
 
@@ -96,12 +102,12 @@ def create_interval_df(vids, df, interval):
 
 def get_frames_interval(vid_path, start_time, end_time, frame_transform=None):
     vid_cap = cv2.VideoCapture(vid_path.as_posix())
-    fps =  vid_cap.get(5)
+    fps = vid_cap.get(5)
     frames = vid_cap.get(7)
-    length = vid_cap.get(7)/vid_cap.get(5)
+    length = vid_cap.get(7) / vid_cap.get(5)
 
-    start_time = round(start_time/0.04)*0.04
-    end_time = round(end_time/0.04)*0.04
+    start_time = round(start_time / 0.04) * 0.04
+    end_time = round(end_time / 0.04) * 0.04
 
     start_frame = timestamp_to_frame[start_time]
     end_frame = timestamp_to_frame[end_time]
@@ -131,25 +137,38 @@ def get_frames_interval(vid_path, start_time, end_time, frame_transform=None):
 
     return torch.stack(frames, 0)
 
+
 def main():
-    vidcap_left = cv2.VideoCapture('/home/colivier-local/Videos/left.mp4')
-    vidcap_right = cv2.VideoCapture('/home/colivier-local/Videos/right.mp4')
+    video_number = 1
+
+    show_image = False
+
+    input_dir = os.path.join(os.environ["HOME"], "Downloads")
+    left_file = os.path.join(input_dir, f"left-{video_number}", ".mp4")
+    right_file = os.path.join(input_dir, f"right-{video_number}", ".mp4")
+    vidcap_left = cv2.VideoCapture(left_file)
+    vidcap_right = cv2.VideoCapture(right_file)
+
     fps = vidcap_left.get(cv2.CAP_PROP_FPS)
     frame_width = int(vidcap_left.get(3))
     frame_height = int(vidcap_left.get(4))
+
     print(f"Video FPS={fps}")
     print(f"Input size: {frame_width} x {frame_height}")
 
-    #fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    fourcc = cv2.VideoWriter_fourcc(*'H264')
+    final_frame_width = frame_width * 2
+    final_frame_height = frame_height
 
-    # final_frame_width = (frame_width * 2)
-    # final_frame_height = frame_height
+    # final_frame_width = (frame_width * 2) // 2
+    # final_frame_height = frame_height // 2
 
-    final_frame_width = (frame_width * 2) // 2
-    final_frame_height = frame_height // 2
-
-    out = cv2.VideoWriter(filename='output.avi', fourcc=fourcc, fps=fps, frameSize=(final_frame_width, final_frame_height))
+    out = cv2.VideoWriter(
+        filename=f"stitrched-output-{video_number}.mov",
+        fourcc=cv2.VideoWriter_fourcc(*"XVID"),
+        fps=fps,
+        frameSize=(final_frame_width, final_frame_height),
+        isColor=True,
+    )
 
     r1, frame1 = vidcap_left.read()
     r2, frame2 = vidcap_right.read()
@@ -157,32 +176,41 @@ def main():
     frame_id = 0
     timer = Timer()
     while r1 and r2:
-        #cv2.imwrite("frames/frame%d.png" % count, image)     # save frame as JPEG file
+        # cv2.imwrite("frames/frame%d.png" % count, image)     # save frame as JPEG file
         timer.tic()
         r1, frame1 = vidcap_left.read()
         r2, frame2 = vidcap_right.read()
 
-        frame1 = cv2.resize(frame1, (final_frame_width//2, final_frame_height))
-        frame2 = cv2.resize(frame2, (final_frame_width//2, final_frame_height))
+        if final_frame_height != frame_height:
+            frame1 = cv2.resize(frame1, (final_frame_width // 2, final_frame_height))
+            frame2 = cv2.resize(frame2, (final_frame_width // 2, final_frame_height))
 
         # Concatenate the frames side-by-side
         combined_frame = cv2.hconcat([frame1, frame2])
 
-        # cv2.imshow('Combined Image', combined_frame)
-        # cv2.waitKey(0)
+        if show_image:
+            cv2.imshow('Combined Image', combined_frame)
+            cv2.waitKey(0)
 
         out.write(combined_frame)
 
         timer.toc()
         if frame_id % 20 == 0:
-              print('Processing frame {} ({:.2f} fps)'.format(frame_id, 1. / max(1e-5, timer.average_time)))
+            print(
+                "Processing frame {} ({:.2f} fps)".format(
+                    frame_id, 1.0 / max(1e-5, timer.average_time)
+                )
+            )
         frame_id += 1
 
+    out.release()
     vidcap_left.release()
     vidcap_right.release()
-    out.release()
-    cv2.destroyAllWindows()
-    print("Done.")
+
+    if show_image:
+        cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
+    print("Done.")
