ad67572af270377686225f01ae17937e1ad519ae
Chris Olivier <cjolivier01@apache.org>
2023-09-17 05:58:44 -0700

    ..

--- src/lib/stitch_synchronize.py
+++ src/lib/stitch_synchronize.py
@@ -5,14 +5,17 @@ import numpy as np
 
 def synchronize_by_audio(file1_path: str, file2_path: str):
     # Load the videos
+    print("Openning videos...")
     video1 = mp.VideoFileClip(file1_path)
     video2 = mp.VideoFileClip(file2_path)
 
     # Load audio from the videos
+    print("Loading audio...")
     audio1 = video1.audio.to_soundarray()
     audio2 = video2.audio.to_soundarray()
 
     # Calculate the cross-correlation of audio1 and audio2
+    print("Calculating cross-correlation...")
     correlation = np.correlate(audio1[:, 0], audio2[:, 0], mode='full')
     lag = np.argmax(correlation) - len(audio1) + 1
 
@@ -20,10 +23,14 @@ def synchronize_by_audio(file1_path: str, file2_path: str):
     fps = video1.fps
     time_offset = lag / fps
 
+    print(f"Time offset: {time_offset}")
+
     # Synchronize video2 with video1
+    print("Creating subclip...")
     synchronized_video2 = video2.subclip(max(0, -time_offset), video2.duration)
 
     # Write the synchronized video to a file
+    print("Writing synchronized file...")
     synchronized_video2.write_videofile('synchronized_video2.mp4', codec='libx264')
 
     # Close the videos
@@ -33,6 +40,6 @@ def synchronize_by_audio(file1_path: str, file2_path: str):
 if __name__ == "__main__":
     video_number = 0
     synchronize_by_audio(
-        file1_path=f"/home/{os.environ['HOME']}/Videos/left-{video_number}.mp4",
-        file2_path=f"/home/{os.environ['HOME']}/Videos/right-{video_number}.mp4",
+        file1_path=f"{os.environ['HOME']}/Videos/left-{video_number}.mp4",
+        file2_path=f"{os.environ['HOME']}/Videos/right-{video_number}.mp4",
     )
