7f317f175a260341feba8b48e033874c03a921fe
Chris Olivier <cjolivier01@apache.org>
2023-09-24 06:47:27 -0700

    ..

--- hockeymom/csrc/stitcher/HmStitcher.cpp
+++ hockeymom/csrc/stitcher/HmStitcher.cpp
@@ -1,2 +1,2 @@
 #include "HmStitcher.h"
-
+#include "hugin/"
--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
+
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
+    def __init__(self, name, sourcedir=""):
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
@@ -17,13 +18,17 @@ class CMakeExtension(Extension):
 class CMakeBuild(build_ext):
     def run(self):
         try:
-            out = subprocess.check_output(['cmake', '--version'])
+            out = subprocess.check_output(["cmake", "--version"])
         except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
+            raise RuntimeError(
+                "CMake must be installed to build the following extensions: "
+                + ", ".join(e.name for e in self.extensions)
+            )
 
-        cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-        if cmake_version < LooseVersion('3.5.0'):
+        cmake_version = LooseVersion(
+            re.search(r"version\s*([\d.]+)", out.decode()).group(1)
+        )
+        if cmake_version < LooseVersion("3.5.0"):
             raise RuntimeError("CMake >= 3.5.0 is required")
 
         for ext in self.extensions:
@@ -31,52 +36,76 @@ class CMakeBuild(build_ext):
 
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
+            "-DPYTHON_EXECUTABLE=" + sys.executable,
+        ]
 
-        build_type = os.environ.get("BUILD_TYPE", "Release")
-        build_args = ['--config', build_type]
+        default_build_type = "Release"
+        if int(os.environ.get("DEBUG", "0")) > 0:
+            default_build_type = "Debug"
+
+        build_type = os.environ.get("BUILD_TYPE", default_build_type)
+        build_args = ["--config", build_type]
+
+        print(f"Build type: {build_type}")
 
         # Pile all .so in one place and use $ORIGIN as RPATH
         cmake_args += ["-DCMAKE_BUILD_WITH_INSTALL_RPATH=TRUE"]
         cmake_args += ["-DCMAKE_INSTALL_RPATH={}".format("$ORIGIN")]
 
         if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(build_type.upper(), extdir)]
+            cmake_args += [
+                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(
+                    build_type.upper(), extdir
+                )
+            ]
             if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
+                cmake_args += ["-A", "x64"]
+            build_args += ["--", "/m"]
         else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + build_type]
-            build_args += ['--', '-j4']
+            cmake_args += ["-DCMAKE_BUILD_TYPE=" + build_type]
+            build_args += ["--", "-j4"]
 
         env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
+        env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get("CXXFLAGS", ""), self.distribution.get_version()
+        )
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake',
-                               '--build', '.',
-                               '--target', os.path.basename(ext.name),
-                               '--target', 'nona',
-                               '--target', 'pto_gen',
-                               '--target', 'cpfind',
-                               '--target', 'autooptimiser',
-                               ] + build_args,
-                              cwd=self.build_temp)
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env
+        )
+        cmake_targets = [
+            "--target",
+            os.path.basename(ext.name),
+            "--target",
+            "nona",
+            "--target",
+            "pto_gen",
+            "--target",
+            "cpfind",
+            "--target",
+            "autooptimiser",
+        ]
+
+        subprocess.check_call(
+            ["cmake"] + ["--build", "."] + cmake_targets + build_args,
+            cwd=self.build_temp,
+        )
+
 
 setup(
-    name='hockeymom',
-    version='0.1.0',
-    author='Christopher Olivier',
-    author_email='cjolivier01@gmail.com',
-    description='HockeyMOM project',
+    name="hockeymom",
+    version="0.1.0",
+    author="Christopher Olivier",
+    author_email="cjolivier01@gmail.com",
+    description="HockeyMOM project",
     long_description=open("README.rst").read(),
-    ext_modules=[CMakeExtension('hockeymom/_hockeymom')],
+    ext_modules=[CMakeExtension("hockeymom/_hockeymom")],
     packages=find_packages(),
     cmdclass=dict(build_ext=CMakeBuild),
     url="https://github.com/cjolivier01/hockeymom2",
     zip_safe=False,
-    install_requires=[]
+    install_requires=[],
 )
