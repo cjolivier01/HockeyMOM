f881f178a0a065c21d805709d611f663a8ba6bf9
Christopher Olivier <colivier@tesla.com>
2023-09-26 14:43:53 -0700

    ...

--- hockeymom/csrc/dataloader/StitchedDataLoader.cpp
+++ hockeymom/csrc/dataloader/StitchedDataLoader.cpp
@@ -2,9 +2,24 @@
 
 namespace hm {
 
-StitchingDataLoader::StitchingDataLoader() {}
-StitchingDataLoader::~StitchingDataLoader() {}
-
+StitchingDataLoader::StitchingDataLoader(
+    std::size_t start_frame_id,
+    std::size_t thread_count)
+    : next_frame_id_(start_frame_id),
+      thread_count_(thread_count),
+      input_queue_ <
+          std::make_unique<
+              moodycamel::BlockingConcurrentQueue<std::unique_ptr<FrameData>>>(/*capacity=*/kInputQueueCapacity) {
 }
 
+StitchingDataLoader::~StitchingDataLoader() {
+  shutdown();
+}
 
+void StitchingDataLoader::shutdown() {}
+
+void StitchingDataLoader::add_frame(
+    std::size_t frame_id,
+    std::vector<std::shared_ptr<MatrixRGB>>&& images) {}
+
+} // namespace hm
--- hockeymom/csrc/dataloader/StitchedDataLoader.h
+++ hockeymom/csrc/dataloader/StitchedDataLoader.h
@@ -1,20 +1,39 @@
 #pragma once
 
-//#include <opencv2/opencv.hpp>
-//#include <opencv2/highgui/highgui.hpp>
-//#include "opencv2/videoio.hpp"
+#include "hockeymom/csrc/common/MatrixRGB.h"
+
+#include "concurrentqueue/blockingconcurrentqueue.h"
 
 #include <string>
 #include <vector>
 
 namespace hm {
 
-class StitchingDataLoader {
- public:
-  StitchingDataLoader();
-  ~StitchingDataLoader();
- private:
+struct FrameData {
+  static constexpr std::size_t kInvalidFrameId =
+      std::numeric_limits<std::size_t>::max();
+  std::size_t frame_id{kInvalidFrameId};
+  std::vector<std::shared_ptr<MatrixRGB>> images;
 };
 
-}
+class StitchingDataLoader {
+  static constexpr std::size_t kInputQueueCapacity = 32;
+ public:
+  StitchingDataLoader(std::size_t start_frame_id, std::size_t thread_count);
+  ~StitchingDataLoader();
 
+  void add_frame(
+      std::size_t frame_id,
+      std::vector<std::shared_ptr<MatrixRGB>>&& images);
+
+ private:
+  void shutdown();
+
+  std::size_t next_frame_id_{0};
+  std::size_t thread_count_;
+  std::unique_ptr<
+      moodycamel::BlockingConcurrentQueue<std::unique_ptr<FrameData>>>
+      input_queue_;
+};
+
+} // namespace hm
