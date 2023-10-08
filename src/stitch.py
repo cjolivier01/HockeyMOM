d801eef9c471c4f03dfa9d7942dd1e540d0716ef
Chris Olivier <cjolivier01@apache.org>
2023-10-07 18:49:55 -0700

    ..

--- src/stitch.py
+++ src/stitch.py
@@ -79,7 +79,7 @@ def stitch_videos(
     start_frame_number = 200
     # start_frame_number = 0
 
-    max_frames = 50
+    max_frames = 100
 
     output_stitched_video_file = "./stitched_output.avi"
 
