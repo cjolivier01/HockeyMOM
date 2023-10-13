95599e625b09d5550a6250beb5483a5482980eff
Chris Olivier <cjolivier01@apache.org>
2023-10-13 15:10:02 -0700

    removed

--- mixsort_run.sh
+++ mixsort_run.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-PYTHONPATH="$(pwd)/build:$(pwd)/src:$(pwd)/models/mixsort:$(pwd)/models/mixsort/MixViT" \
-  python src/hm_track_mixsort.py -expn=my_experiment -f=models/mixsort/exps/example/mot/yolox_x_sportsmot.py -c=models/mixsort/pretrained/yolox_x_sports_train.pth -b=1 -d=1 --config=track
+
+EXPERIMENT_FILE="models/mixsort/exps/example/mot/yolox_x_sportsmot.py"
+PRETRAINED_MODEL="pretrained/mixsort/yolox_x_sports_train.pth"
+
+PYTHONPATH="$(pwd)/build:$(pwd)/models/mixsort:$(pwd)/models/mixsort/MixViT:$(pwd)/src" \
+  python src/hm_track_mixsort.py \
+  -expn=my_experiment \
+  -f="${EXPERIMENT_FILE}" \
+  -c="${PRETRAINED_MODEL}" \
+  -b=1 -d=1 \
+  --min-box-area=55 \
+  --config=track
