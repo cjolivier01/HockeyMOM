d92ff759c491ba28d960e43724880e109b751e9d
Chris Olivier <cjolivier01@apache.org>
2023-10-18 07:02:54 -0700

    ..

--- mixsort_run.sh
+++ mixsort_run.sh
@@ -5,9 +5,14 @@ PRETRAINED_MODEL="models/mixsort/pretrained/yolox_x_sports_train.pth"
 #PRETRAINED_MODEL="pretrained/mixsort/latest_ckpt.pth.tar"
 VIDEO="${HOME}/Videos/roseville/Sacramento.10.15.2023.mp4"
 
+#EXP_NAME="mixsort-run-$(uuidgen)"
+EXP_NAME="mixsort-run"
+
+echo "Experiment name: ${EXP_NAME}"
+
 PYTHONPATH="$(pwd)/build:$(pwd)/models/mixsort:$(pwd)/models/mixsort/MixViT:$(pwd)/src" \
   python src/hm_track_mixsort.py \
-  -expn=my_experiment \
+  -expn="${EXP_NAME}" \
   -f="${EXPERIMENT_FILE}" \
   -c="${PRETRAINED_MODEL}" \
   -b=1 -d=1 \
