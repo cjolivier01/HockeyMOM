fb6d3e73cdc14ba0e9803bfa0709284a5d6f47e0
Chris Olivier <cjolivier01@apache.org>
2023-11-25 05:38:57 -0800

    withstitch

--- mixsort_run_vallco.sh
+++ mixsort_run_vallco.sh
@@ -28,8 +28,8 @@ MIXFORMER_SCRIPT="mixformer_deit_ch_ht"
 #VIDEO="${HOME}/src/datasets/hockeyTrackingDataset/clips/PIT_vs_WAS_2016/001.mp4"
 #VIDEO="${HOME}/Videos/lbd2/stitched_output-with-audio.avi"
 #VIDEO="${HOME}/Videos/blackhawks/stitched_output-with-audio.avi"
-VIDEO="${HOME}/Videos/tvbb/stitched_output-with-audio.avi"
-#VIDEO="${HOME}/Videos/tvbb"
+#VIDEO="${HOME}/Videos/tvbb/stitched_output-with-audio.avi"
+VIDEO="${HOME}/Videos/tvbb"
 
 #EXP_NAME="mixsort-run-$(uuidgen)"
 EXP_NAME="mixsort-run-vallco"
@@ -53,7 +53,7 @@ OMP_NUM_THREADS=16 \
   -expn="${EXP_NAME}" \
   -f="${EXPERIMENT_FILE}" \
   -c="${PRETRAINED_MODEL}" \
-  -b=48 -d=1 \
+  -b=4 -d=1 \
   --infer \
   --start-frame=${START_FRAME} \
   ${HYPER_PARAMS} ${STITCHING_PARAMS} \
