1b0d496092aa68096074522bfb2835b907d32eee
Chris Olivier <cjolivier01@apache.org>
2023-11-25 16:27:45 -0800

    ..

--- mixsort_run_cvat.sh
+++ mixsort_run_cvat.sh
@@ -1,48 +1,23 @@
 #!/bin/bash
 
-#EXPERIMENT_FILE="models/mixsort/exps/example/mot/yolox_x_sportsmot.py"
-#EXPERIMENT_FILE="models/mixsort/exps/example/mot/yolox_x_hockey.py"
-#EXPERIMENT_FILE="models/mixsort/exps/example/mot/yolox_x_ch_ht.py"
 EXPERIMENT_FILE="models/mixsort/exps/example/mot/yolox_x_ch_cvat.py"
 #
 # Models
 #
-#PRETRAINED_MODEL="pretrained/yolox/yolox_x_sports_train.pth"
-#PRETRAINED_MODEL="pretrained/yolox/yolox_x_ch.pth"
 PRETRAINED_MODEL="pretrained/yolox/my_ch.pth.tar"
-#PRETRAINED_MODEL="pretrained/yolox/yolox_x_my_ch_to_hockey_tracking_dataset.pth.tar"
-#PRETRAINED_MODEL=""
 
 MIXFORMER_SCRIPT="mixformer_deit_hockey"
-#MIXFORMER_SCRIPT="mixformer_deit_ch_ht"
 
 #
 # Videos
 #
-#VIDEO="${HOME}/Videos/roseville/Sacramento.10.15.2023.mp4"
-#VIDEO="/mnt/data/Videos/roseville/stitched_output-with-audio.avi"
-#VIDEO="${HOME}/src/datasets/hockeyTrackingDataset/clips/CHI_VS_TOR/003.mp4"i
-#VIDEO="/mnt/data/Videos/roseville/clips/at10mins_small.mp4"
-#VIDEO="${HOME}/Videos/roseville/clips/at10mins_small.avi"
-#VIDEO="${HOME}/Videos/roseville/clips/at10mins_small.mp4"
-#VIDEO="/mnt/data/Videos/SportsMOT/v_00HRwkvvjtQ_c001.mp4"
-#VIDEO="${HOME}/src/datasets/hockeyTrackingDataset/clips/PIT_vs_WAS_2016/001.mp4"
-#VIDEO="${HOME}/Videos/lbd2/stitched_output-with-audio.avi"
-#VIDEO="${HOME}/Videos/blackhawks/stitched_output-with-audio.avi"
-#VIDEO="${HOME}/Videos/tvbb/stitched_output-with-audio.avi"
 VIDEO="${HOME}/Videos/tvbb/right.mp4"
-#VIDEO="${HOME}/Videos/tvbb"
 
 EXP_NAME="mixsort-run-cvat"
 
-#START_FRAME=0
-#START_FRAME=1900
-#START_FRAME=2900
 START_FRAME=6200
 MAX_FRAMES=200
 HYPER_PARAMS="--conf=0.001 --track_thresh=0.01 --track_thresh_low=0.005"
-
-#STITCHING_PARAMS="--lfo=15.392 --rfo=0"
 
 echo "Experiment name: ${EXP_NAME}"
 
@@ -58,7 +33,8 @@ OMP_NUM_THREADS=16 \
   --cvat-output \
   --start-frame=${START_FRAME} \
   --max-frames=${MAX_FRAMES} \
-  ${HYPER_PARAMS} ${STITCHING_PARAMS} \
+  ${HYPER_PARAMS} \
+  --save-frame-dir="$(pwd)/YOLOX_outputs/${EXP_NAME}/frames" \
   --min-box-area=35 \
   --rink="vallco" \
   --config=track \
