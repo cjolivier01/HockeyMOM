0fd467e4d897c5c5d216ef0285a58848b95f0a5e
Christopher Olivier <cjolivier01@gmail.com>
2023-12-20 15:10:36 -0800

    ..

--- experiments/crowdhuman_dla34.sh
+++ experiments/crowdhuman_dla34.sh
@@ -1,5 +1,8 @@
+#LD_LIBRARY_PATH=/home/colivier/.conda/envs/ubuntu-gpu/lib:/apcv/shared/cuda/cudnn/8.2.0.37/lib64:/apcv/shared/conda-envs/ai-1555/lib:/apcv/shared/conda-envs/ai-1555/lib/x86_64-linux-gnu:/cm/local/apps/cuda/libs/470.199.02-0ubuntu0.20.04.1/lib64:/cm/shared/apps/cuda11.4/toolkit/11.4.0/targets/x86_64-linux/lib
+
 cd src
-PYTHONPATH="$(pwd):$(pwd)/hmlib:$(pwd)/../models/mixsort:${PYTHONPATH}" \
-  LD_LIBRARY_PATH=/home/colivier/.conda/envs/ubuntu-gpu/lib:/apcv/shared/cuda/cudnn/8.2.0.37/lib64:/apcv/shared/conda-envs/ai-1555/lib:/apcv/shared/conda-envs/ai-1555/lib/x86_64-linux-gnu:/cm/local/apps/cuda/libs/470.199.02-0ubuntu0.20.04.1/lib64:/cm/shared/apps/cuda11.4/toolkit/11.4.0/targets/x86_64-linux/lib \
-  python train.py mot --exp_id crowdhuman_dla34 --gpus 0 --batch_size 9 --load_model '../pretrained/dla34/ctdet_coco_dla_2x.pth' --num_epochs 60 --print_iter 25 --lr_step '50' --data_cfg '../src/hmlib/cfg/crowdhuman.json' $@
+PYTHONPATH="$(pwd)/hmlib" \
+  python \
+  train.py mot \
+  --exp_id crowdhuman_dla34 --gpus 0 --batch_size 9 --load_model '../pretrained/dla34/ctdet_coco_dla_2x.pth' --num_epochs 60 --print_iter 25 --lr_step '50' --data_cfg '../src/hmlib/cfg/crowdhuman.json' $@
 cd ..
