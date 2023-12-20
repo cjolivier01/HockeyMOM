2da1d43df2527e3d44af4ec92fa296605c2d690f
Christopher Olivier <cjolivier01@gmail.com>
2023-12-20 13:59:38 -0800

    fan speed control

--- hm_run_sharks_orange.sh
+++ hm_run_sharks_orange.sh
@@ -12,7 +12,7 @@ MIXFORMER_SCRIPT="mixformer_deit_hockey"
 #
 # Videos
 #
-VIDEO="${HOME}/Videos/sharksbb1-2"
+VIDEO="${HOME}/Videos/sharksbb1-2/stiched_output-with-audio.avi"
 
 EXP_NAME="$(basename $0 .sh)"
 
--- scripts/gpu-fan-control.py
+++ scripts/gpu-fan-control.py
@@ -1,8 +1,8 @@
-import os
 import subprocess
 import pyipmi
 import pyipmi.interfaces
 import pyipmi.sensor
+import time
 
 GPU_FANS = {
     "GPU3 Temp": ["FANA"],
@@ -15,11 +15,19 @@ GPU_FANS = {
 # set fan in peripheral zone to 25%: ipmitool raw 0x30 0x70 0x66 0x01 0x01 0x16
 #
 
+FAST_FAN_TEMP = 55
+SLOW_FAN_TEMP = 45
+
 ZONE_CPU = 0
 ZONE_PERIPHERAL = 1
 
+FAN_MODE_STANDARD = 0
+FAN_MODE_FULL = 1
+FAN_MODE_OPTIMAL = 2
+FAN_MODE_HEAVY_IO = 4
 
-def set_zone_fan_speed(ipmi: pyipmi.Ipmi, speed_percent, zone: int = 1):
+
+def set_zone_fan_speed(speed_percent, zone: int = 1):
     ratio = speed_percent / 100
     fan_speed = int(ratio * 64)
     raw_command = [0x30, 0x70, 0x66, 0x01, int(zone), int(fan_speed)]
@@ -30,61 +38,63 @@ def set_zone_fan_speed(ipmi: pyipmi.Ipmi, speed_percent, zone: int = 1):
     subprocess.check_call(cmd)
 
 
-def main(
-    ipmi_addr: str = "192.168.1.60", user: str = "ADMIN", password: str = "Pal0Alt0"
-):
-    ipmi = None
-    try:
-        interface = pyipmi.interfaces.create_interface(
-            "ipmitool", interface_type="open"
-        )
-        ipmi = pyipmi.create_connection(interface)
-        ipmi.session.set_auth_type_user(user, password)
-        ipmi.session.establish()
-        ipmi.target = pyipmi.Target(ipmb_address=0x20)
+def set_fan_mode(fan_mode: int):
+    raw_command = [
+        0x30,
+        0x45,
+    ]
+    cmd = [
+        "ipmitool",
+        "raw",
+    ] + [hex(v) for v in raw_command]
+    subprocess.check_call(cmd)
 
-        for selector in range(1, 6):
-            caps = ipmi.get_dcmi_capabilities(selector)
-            print("Selector: {} ".format(selector))
-            print("  version:  {} ".format(caps.specification_conformence))
-            print("  revision: {}".format(caps.parameter_revision))
-            print("  data:     {}".format(caps.parameter_data))
 
-        rsp = ipmi.get_power_reading(1)
+def main():
+    mode = "slow"
+    while True:
+        ipmi = None
+        try:
+            interface = pyipmi.interfaces.create_interface(
+                "ipmitool", interface_type="open"
+            )
+            ipmi = pyipmi.create_connection(interface)
+            ipmi.session.establish()
+            ipmi.target = pyipmi.Target(ipmb_address=0x20)
 
-        print("Power Reading")
-        print("  current:   {}".format(rsp.current_power))
-        print("  minimum:   {}".format(rsp.minimum_power))
-        print("  maximum:   {}".format(rsp.maximum_power))
-        print("  average:   {}".format(rsp.average_power))
-        print("  timestamp: {}".format(rsp.timestamp))
-        print("  period:    {}".format(rsp.period))
-        print("  state:     {}".format(rsp.reading_state))
+            max_temp = 0
 
-        gpus = []
-        fans = []
+            reservation_id = ipmi.reserve_device_sdr_repository()
+            for sdr in ipmi.get_repository_sdr_list(reservation_id):
+                if (
+                    sdr.device_id_string.startswith("GPU")
+                    and "Temp" in sdr.device_id_string
+                ):
+                    reading = ipmi.get_sensor_reading(sdr.number)
+                    temp_in_c = reading[0]
+                    max_temp = max(max_temp, temp_in_c)
+                    print(f"{sdr.device_id_string}: {temp_in_c} degrees C")
+                else:
+                    continue
+            
+            print(f"\nMax GPU temp: {max_temp} degrees C current mode is {mode})")
+            set_zone_fan_speed(speed_percent=50, zone=ZONE_CPU)
+            if max_temp >= FAST_FAN_TEMP: # and mode == "slow":
+                set_zone_fan_speed(speed_percent=100, zone=ZONE_PERIPHERAL)
+                mode = "fast"
+            elif max_temp < SLOW_FAN_TEMP: # and mode == "fast":
+                set_zone_fan_speed(speed_percent=50, zone=ZONE_PERIPHERAL)
+                mode = "slow"
+            time.sleep(5)
 
-        reservation_id = ipmi.reserve_device_sdr_repository()
-        for sdr in ipmi.get_repository_sdr_list(reservation_id):
-            if (
-                sdr.device_id_string.startswith("GPU")
-                and "Temp" in sdr.device_id_string
-            ):
-                print(sdr)
-                gpus.append(sdr)
-            elif "FAN" in sdr.device_id_string:
-                print(sdr)
-                fans.append(sdr)
-            else:
-                continue
+        except Exception as e:
+            print(f"IPMI error: {e}")
+        finally:
+            if ipmi is not None:
+                ipmi.session.close()
 
-        set_zone_fan_speed(ipmi, speed_percent=50, zone=ZONE_PERIPHERAL)
-
-    except Exception as e:
-        print(f"IPMI error: {e}")
-    finally:
-        if ipmi is not None:
-            ipmi.session.close()
+def mamage_gpu_fans():
+    main()
 
 if __name__ == "__main__":
     main()
