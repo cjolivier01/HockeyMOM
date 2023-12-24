670235f4083f98371b2ef5f72ac6696a26942ad1
Christopher Olivier <cjolivier01@gmail.com>
2023-12-24 08:12:43 -0800

    prefer

--- scripts/gpu-fan-control.py
+++ scripts/gpu-fan-control.py
@@ -4,16 +4,12 @@ import pyipmi.interfaces
 import pyipmi.sensor
 import time
 
-from typing import Tuple
-
 # setting fan speeds
 #
 # https://forums.servethehome.com/index.php?resources/supermicro-x9-x10-x11-fan-speed-control.20/
 #
 # set fan in peripheral zone to 25%: ipmitool raw 0x30 0x70 0x66 0x01 0x01 0x16
 #
-
-SENSOR_TYPE_TEMPERATURE = 1
 
 PERIPHERAL_FAST_FAN_TEMP = 75
 PERIPHERAL_SLOW_FAN_TEMP = 65
@@ -26,24 +22,6 @@ FAN_MODE_STANDARD = 0
 FAN_MODE_FULL = 1
 FAN_MODE_OPTIMAL = 2
 FAN_MODE_HEAVY_IO = 4
-
-
-ZONE_ITEMS = {
-    "CPU Temp": ZONE_CPU,
-    "System Temp": ZONE_CPU,
-    "Peripheral Temp": ZONE_PERIPHERAL,
-    "CPU_VRM Temp": ZONE_CPU,
-    "SOC_VRM Temp": ZONE_CPU,
-    "VRMABCD Temp": ZONE_CPU,
-    "VRMEFGH Temp": ZONE_CPU,
-}
-
-
-# These have their own fan
-IGNORE_TEMPS = {
-    # Ryzen
-    "SOC_VRM Temp",
-}
 
 
 def set_zone_fan_speed(speed_percent, zone: int = 1):
@@ -70,93 +48,47 @@ def set_fan_mode(fan_mode: int):
     subprocess.check_call(cmd)
 
 
-def get_percent(
-    sdr: pyipmi.sdr.SdrFullSensorRecord,
-    reading: Tuple[int],
-    normal_maximum_override: int = None,
-):
-    current_temp = reading[0]
-    if normal_maximum_override is not None:
-        maximum = normal_maximum_override
-    elif sdr.device_id_string.startswith("GPU"):
-        maximum = 80
-    else:
-        maximum = sdr.normal_maximum
-    range = maximum - sdr.nominal_reading
-    assert range > 0
-    adjust_nominal_amount = -5
-    temp_position = current_temp - (sdr.nominal_reading + adjust_nominal_amount)
-    if temp_position < 0:
-        # below nominal
-        return 0.0
-    return (temp_position / range) * 100
+def manage_temp(ipmi: pyipmi.Ipmi, match_str: str, zone: int, current_mode: str):
+    max_temp = 0
 
-
-def adjust_percent(pct: int):
-    return min(pct + 10, 100)
-
-
-def manage_temp(ipmi: pyipmi.Ipmi):
-    max_temps = {
-        ZONE_PERIPHERAL: 0.0,
-        ZONE_CPU: 0.0,
-    }
-    max_percents = {
-        ZONE_PERIPHERAL: 0.0,
-        ZONE_CPU: 0.0,
-    }
     sensors = []
     reservation_id = ipmi.reserve_device_sdr_repository()
     for sdr in ipmi.get_repository_sdr_list(reservation_id):
-        if "Temp" not in sdr.device_id_string:
-            continue
-        sensors.append(sdr)
-        if len(sensors) == 1:
-            print("")
-        try:
+        if match_str in sdr.device_id_string:
+            sensors.append(sdr)
+            if len(sensors) == 1:
+                print("")
             reading = ipmi.get_sensor_reading(sdr.number)
-        except pyipmi.CompletionCodeError as e:
-            if e.cc == 203:
-                # Requested data not present (device isn't there/plugged in)
-                continue
-            raise
-        if sdr.device_id_string.startswith("GPU"):
-            zone = ZONE_PERIPHERAL
+            temp_in_c = reading[0]
+            max_temp = max(max_temp, temp_in_c)
+            print(f"{sdr.device_id_string}: {temp_in_c} degrees C")
         else:
-            zone = ZONE_ITEMS[sdr.device_id_string]
-        temp_in_c = reading[0]
-        max_temps[zone] = max(max_temps[zone], temp_in_c)
-        # if sdr.device_id_string == "SOC_VRM Temp":
-        #     print(sdr)
-        #     continue
-        percent_high = get_percent(sdr, reading)
-        max_percents[zone] = max(percent_high, max_percents[zone])
-        print(f"{sdr.device_id_string}: {temp_in_c} degrees C, percent: {percent_high}")
+            continue
 
-    set_zone_fan_speed(
-        speed_percent=adjust_percent(max_percents[ZONE_PERIPHERAL]),
-        zone=ZONE_PERIPHERAL,
-    )
-    set_zone_fan_speed(
-        speed_percent=adjust_percent(max_percents[ZONE_CPU]), zone=ZONE_CPU
-    )
+    if max_temp <= PERIPHERAL_SUPER_SLOW_FAN_TEMP:
+        if current_mode != "super-slow":
+            set_zone_fan_speed(speed_percent=25, zone=zone)
+            current_mode = "super-slow"
+    elif max_temp <= PERIPHERAL_SLOW_FAN_TEMP:
+        if current_mode != "slow":
+            set_zone_fan_speed(speed_percent=40, zone=zone)
+            current_mode = "slow"
+    elif max_temp <= PERIPHERAL_FAST_FAN_TEMP:
+        if current_mode != "medium":
+            set_zone_fan_speed(speed_percent=60, zone=zone)
+            current_mode = "medium"
+    else:
+        if current_mode != "fast":
+            set_zone_fan_speed(speed_percent=100, zone=zone)
+            current_mode = "fast"
 
-    print(
-        f"Zone Peripheral: {max_temps[ZONE_PERIPHERAL]} "
-        f"degrees C, max percent: {max_percents[ZONE_PERIPHERAL]}%, "
-        f"speed percent {adjust_percent(max_percents[ZONE_PERIPHERAL])})"
-    )
-    print(
-        f"Zone CPU: {max_temps[ZONE_CPU]} "
-        f"degrees C, max percent: {max_percents[ZONE_CPU]}%, "
-        f"speed percent {adjust_percent(max_percents[ZONE_CPU])})"
-    )
+    print(f"Max {match_str} temp: {max_temp} degrees C current mode is {current_mode})")
+    return current_mode
 
 
 def main():
-    # Start high speed in case we error out, we stay at highest setting (which is noticeable)
     set_zone_fan_speed(speed_percent=100, zone=ZONE_PERIPHERAL)
-    set_zone_fan_speed(speed_percent=100, zone=ZONE_CPU)
+    gpu_mode = "fast"
 
     while True:
         ipmi = None
@@ -169,7 +101,9 @@ def main():
             ipmi.session.establish()
             ipmi.target = pyipmi.Target(ipmb_address=0x20)
 
-            manage_temp(ipmi=ipmi)
+            gpu_mode = manage_temp(
+                ipmi=ipmi, match_str="GPU", zone=ZONE_PERIPHERAL, current_mode=gpu_mode
+            )
 
             time.sleep(10)
 
