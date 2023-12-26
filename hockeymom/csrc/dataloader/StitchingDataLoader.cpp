310eb7a3aa268ae871b2c18badc37256c4826c88
Christopher Olivier <cjolivier01@gmail.com>
2023-12-25 16:08:21 -0800

    ..

--- hockeymom/csrc/dataloader/StitchingDataLoader.cpp
+++ hockeymom/csrc/dataloader/StitchingDataLoader.cpp
@@ -6,8 +6,8 @@
 
 namespace hm {
 
-//#define FAKE_REMAP
-//#define FAKE_BLEND
+//#define FAKE_REMAP  // ~4 fps
+//#define FAKE_BLEND   // ~10 fps
 
 StitchingDataLoader::StitchingDataLoader(
     std::size_t start_frame_id,
