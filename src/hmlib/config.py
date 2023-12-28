5fd93a30c01c1bf013bd74b657d80e323d4f1f20
Christopher Olivier <cjolivier01@gmail.com>
2023-12-28 04:40:25 -0800

    ..

--- src/hmlib/config.py
+++ src/hmlib/config.py
@@ -2,8 +2,10 @@ import os
 import yaml
 
 
-def get_game_config(game_id: str, root_dir: str):
-    yaml_file_path = os.path.join(root_dir, "config", "games", game_id + ".yaml")
+def get_config(root_dir: str, config_type: str, config_name: str):
+    yaml_file_path = os.path.join(
+        root_dir, "config", config_type, config_name + ".yaml"
+    )
     if os.path.exists(yaml_file_path):
         with open(yaml_file_path, "r") as file:
             try:
@@ -12,18 +14,18 @@ def get_game_config(game_id: str, root_dir: str):
             except yaml.YAMLError as exc:
                 print(exc)
     return {}
+
+
+def get_game_config(game_id: str, root_dir: str):
+    return get_config(root_dir=root_dir, config_type="games", config_name=game_id)
 
 
 def get_rink_config(rink: str, root_dir: str):
-    yaml_file_path = os.path.join(root_dir, "config", "rinks", rink + ".yaml")
-    if os.path.exists(yaml_file_path):
-        with open(yaml_file_path, "r") as file:
-            try:
-                yaml_content = yaml.safe_load(file)
-                return yaml_content
-            except yaml.YAMLError as exc:
-                print(exc)
-    return {}
+    return get_config(root_dir=root_dir, config_type="rinks", config_name=rink)
+
+
+def get_camera_config(game_id: str, root_dir: str):
+    return get_config(root_dir=root_dir, config_type="camera", config_name=game_id)
 
 
 def get_clip_box(game_id: str, root_dir: str):
