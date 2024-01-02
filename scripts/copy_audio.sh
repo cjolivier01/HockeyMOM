c69f08c38270cdcfc5cd07334ab44d76acef6f9a
Chris Olivier <cjolivier01@apache.org>
2024-01-02 04:54:06 -0800

    no loss

--- scripts/copy_audio.sh
+++ scripts/copy_audio.sh
@@ -7,11 +7,8 @@ OUTPUT_VIDEO_WITH_AUDIO="$3"
 ffmpeg \
   -i "${INPUT_VIDEO_WITH_AUDIO}" \
   -i "${INPUT_VIDEO_NO_AUDIO}" \
-  -c:v copy -c:a aac -strict experimental \
+  -c:v copy -c:a copy \
   -map 1:v:0 \
   -map 0:a:0 \
-  -b:v 4000k \
-  -b:a 192k \
   -shortest \
   "${OUTPUT_VIDEO_WITH_AUDIO}"
-
