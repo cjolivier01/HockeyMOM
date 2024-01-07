746de1bbb47d5ee783c063516f8c905d78827368
Christopher Olivier <cjolivier01@gmail.com>
2024-01-07 07:18:25 -0800

    make hm_run one

--- hm_run.sh
+++ hm_run.sh
@@ -1,49 +1,67 @@
 #!/bin/bash
 
-#EXPERIMENT_FILE="models/mixsort/exps/example/mot/yolox_x_sportsmot.py"
-EXPERIMENT_FILE="models/mixsort/exps/example/mot/yolox_x_hockey.py"
+EXPERIMENT_FILE="models/mixsort/exps/example/mot/yolox_x_ch.py"
 
 #
 # Models
 #
-#PRETRAINED_MODEL="models/mixsort/pretrained/yolox_x_sports_train.pth"
-#PRETRAINED_MODEL="pretrained/mixsort/latest_ckpt.pth.tar"
-#PRETRAINED_MODEL="pretrained/mixsort/last_epoch_ckpt.pth.tar"
-#PRETRAINED_MODEL="models/mixsort/pretrained/yolox_x_ch.pth"
-PRETRAINED_MODEL="pretrained/mixsort/my_ch.pth.tar"
-#PRETRAINED_MODEL="pretrained/mixsort/yolox_x_my_ch_to_hockey_tracking_dataset.pth.tar"
+PRETRAINED_MODEL="./pretrained/dla34/crowdhuman_dla34.pth"
 
 MIXFORMER_SCRIPT="mixformer_deit_hockey"
 
 #
 # Videos
 #
-#VIDEO="${HOME}/Videos/roseville/Sacramento.10.15.2023.mp4"
-#VIDEO="/mnt/data/Videos/roseville/stitched_output-with-audio.avi"
-#VIDEO="${HOME}/src/datasets/hockeyTrackingDataset/clips/CHI_VS_TOR/003.mp4"
-#VIDEO="/mnt/data/Videos/roseville/clips/at10mins_small.mp4"
-#VIDEO="${HOME}/Videos/roseville/clips/at10mins_small.avi"
-#VIDEO="${HOME}/Videos/roseville/clips/at10mins_small.mp4"
-#VIDEO="/mnt/data/Videos/SportsMOT/v_00HRwkvvjtQ_c001.mp4"
-#VIDEO="${HOME}/src/datasets/hockeyTrackingDataset/clips/PIT_vs_WAS_2016/001.mp4"
-VIDEO="${HOME}/Videos/lbd2/stitched_output-with-audio.mp4"
-#VIDEO="${HOME}/Videos/blackhawks/stitched_output-with-audio.avi"
+#VIDEO="${HOME}/Videos/lbd2/stitched_output-with-audio.avi"
+#VIDEO="${HOME}/Videos/stockton2/stitched_output-with-audio.avi"
+#VIDEO="${HOME}/Videos/stockton2/"
+VIDEO="${HOME}/Videos/sharksbb1-1/stitched_output-with-audio.avi"
+#VIDEO="${HOME}/Videos/sharksbb1-1"
 
-#EXP_NAME="mixsort-run-$(uuidgen)"
-EXP_NAME="mixsort-run"
+EXP_NAME="$(basename $0 .sh)"
+
+GAME_ID="--game-id sharksbb1-1"
+
+#TEST_SIZE_ARG="--test-size=200x520"
+TEST_SIZE_ARG="--test-size=300x780"
+#TEST_SIZE_ARG="--test-size=400x1040"
+
+START_FRAME=0
+#START_FRAME=1900
+#START_FRAME=2900
+#START_FRAME=8730
+#START_FRAME=47000
+#START_FRAME=16890
+
+#TRACKER="hm"
+TRACKER="fair"
+#BATCH_SIZE=32
+BATCH_SIZE=16
+
+#STITCHING_PARAMS="--lfo=42.63559569682018 --rfo=0"
 
 echo "Experiment name: ${EXP_NAME}"
 
-PYTHONPATH="$(pwd)/build:$(pwd)/models/mixsort:$(pwd)/models/mixsort/MixViT:$(pwd)/src" \
+OMP_NUM_THREADS=16 \
+  LD_LIBRARY_PATH="${CONDA_PREFIX}/lib:${LD_LIBRARY_PATH}" \
+  PYTHONPATH="$(pwd)/build:$(pwd)/src/lib:$(pwd)/models/mixsort:$(pwd)/models/mixsort/MixViT:$(pwd)/src" \
   python src/hmtrack.py \
   -expn="${EXP_NAME}" \
   -f="${EXPERIMENT_FILE}" \
   -c="${PRETRAINED_MODEL}" \
-  -b=4 \
-  -d=1 \
+  -b=${BATCH_SIZE} \
+  --gpus=0,1,2 \
   --infer \
-  --conf=0.01 \
+  --tracker=${TRACKER} \
+  --start-frame=${START_FRAME} \
+  ${HYPER_PARAMS} ${STITCHING_PARAMS} ${GAME_ID} ${TEST_SIZE_ARG} \
   --min-box-area=35 \
+  --rink="vallco" \
   --config=track \
+  --cam-ignore-largest \
   --script="${MIXFORMER_SCRIPT}" \
-  --input_video="${VIDEO}"
+  --input_video="${VIDEO}" \
+  $@ \
+  tracking
+
+
