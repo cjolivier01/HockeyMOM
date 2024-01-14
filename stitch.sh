9daff2a60d29baebf5347588cef361840f879367
Christopher Olivier <cjolivier01@gmail.com>
2024-01-13 18:03:23 -0800

    ..

--- stitch.sh
+++ stitch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#VIDEO_DIR="/home/colivier-local/Videos/sharksbb1-2"
-#VIDEO_DIR="/home/colivier-local/Videos/onehockey-sharksbb2"
-VIDEO_DIR="/home/colivier-local/Videos/onehockey-sharks10a"
-GAME_ID="--game-id=onehockey-sharks10a"
-OFFSETS="--lfo=0 --rfo=0.627263892570015"
+GAME_ID="sharks-bb3-1"
+
+VIDEO_DIR="${HOME}/Videos/${GAME_ID}"
+
+#OFFSETS="--lfo=0 --rfo=0.627263892570015"
 
 OMP_NUM_THREADS=16 \
 	PYTHONPATH=$(pwd):$(pwd)/models/mixsort \
-	python src/stitch.py --video_dir="${VIDEO_DIR}" ${GAME_ID} --project_file=autooptimiser_out.pto ${OFFSETS}  $@
+	python src/stitch.py --video_dir="${VIDEO_DIR}" --game-id=${GAME_ID} --project_file=autooptimiser_out.pto ${OFFSETS}  $@
