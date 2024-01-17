881e943e69ab75c5681d6b2d7518a2bb0a196e9c
Christopher Olivier <cjolivier01@gmail.com>
2024-01-17 04:46:35 -0800

    ..

--- hm_run.sh
+++ hm_run.sh
@@ -15,7 +15,7 @@ MIXFORMER_SCRIPT="mixformer_deit_hockey"
 #VIDEO="${HOME}/Videos/lbd2/stitched_output-with-audio.avi"
 #VIDEO="${HOME}/Videos/tvbb-1"
 
-GAME_ID="jrmocks"
+GAME_ID="queens"
 
 TEST_SIZE_ARG="--test-size=300x780"
 
