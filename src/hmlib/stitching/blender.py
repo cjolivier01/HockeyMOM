978d458f05c21f9721753832740e29f660809458
Chris Olivier <cjolivier01@apache.org>
2024-01-21 11:37:12 -0800

    ...

--- src/hmlib/stitching/blender.py
+++ src/hmlib/stitching/blender.py
@@ -19,6 +19,7 @@ import hockeymom.core as core
 from hmlib.tracking_utils.timer import Timer
 from hmlib.video_out import VideoOutput, ImageProcData, resize_image, rotate_image
 from hmlib.video_out import make_visible_image
+from hmlib.video_stream import VideoStreamWriter
 
 from hmlib.stitching.remapper import (
     ImageRemapper,
@@ -351,7 +352,7 @@ def blend_video(
                 if video_out is None:
                     fps = cap_1.get(cv2.CAP_PROP_FPS)
                     if True:
-                        video_out = FastVideoWriter(
+                        video_out = VideoStreamWriter(
                             filename=output_video,
                             fps=fps,
                             height=video_dim_height,
@@ -428,7 +429,7 @@ def blend_video(
                             cv2.waitKey(1)
                     cpu_blended_image = None
                     for i in range(len(my_blended)):
-                        if isinstance(video_out, FastVideoWriter):
+                        if isinstance(video_out, VideoStreamWriter):
                             video_out.append(my_blended)
                             # video_f.write_video_chunk(
                             #     i=0,
@@ -481,144 +482,11 @@ def blend_video(
             timer.tic()
     finally:
         if video_out is not None:
-            if isinstance(video_out, FastVideoWriter):
+            if isinstance(video_out, VideoStreamWriter):
                 video_out.flush()
                 video_out.close()
             else:
                 video_out.stop()
-
-
-class FastVideoWriter:
-    def __init__(
-        self,
-        filename: str,
-        fps: float,
-        width: int,
-        height: int,
-        codec: str,
-        format: str = "bgr24",
-        batch_size: int = 10,
-        bit_rate: int = 44000,
-        device: torch.device = None,
-    ):
-        self._filename = filename
-        self._fps = fps
-        self._width = width
-        self._height = height
-        self._codec = codec
-        self._format = format
-        self._video_out = None
-        self._video_f = None
-        self._device = device
-        assert batch_size >= 1
-        self._batch_size = batch_size
-        self._batch_items = []
-        self._in_flush = False
-        self._codec_config = torchaudio.io.CodecConfig(
-            bit_rate=5000*1024,
-        )
-        self._codec_config.bit_rate = bit_rate
-        self._codec_config = None
-        self._frame_counter = 0
-
-    def __enter__(self):
-        if self._video_f is None:
-            self.open()
-        return self
-
-    def __exit__(self, *args, **kwargs):
-        self.close()
-
-    def _make_proper_permute(self, image: torch.Tensor):
-        if len(image.shape) == 3:
-            if image.shape[-1] == 3 and self._device is not None:
-                image = image.permute(2, 0, 1)
-        else:
-            if image.shape[-1] == 3 and self._device is not None:
-                image = image.permute(0, 3, 1, 2)
-        return image
-
-    def _add_stream(self):
-        # encoder="h264_nvenc", encoder_format="yuv444p", hw_accel="cuda:0"
-        self._video_out.add_video_stream(
-            frame_rate=self._fps,
-            height=self._height,
-            width=self._width,
-            format=self._format,
-            encoder=self._codec,
-            encoder_format="bgr0",
-            encoder_option={
-                "preset": "slow",
-                "maxrate": "10M",
-                "b:v": "50000k",
-            },
-            codec_config=self._codec_config,
-            hw_accel=str(self._device),
-        )
-        print("Video stream added")
-
-    def bgr_to_rgb(self, batch: torch.Tensor):
-        # Assuming batch is a PyTorch tensor of shape [N, C, H, W]
-        # and the channel order is BGR
-        return batch[:, [2, 1, 0], :, :]
-
-    def close(self):
-        # self.flush()
-        if self._video_f is not None:
-            # self._video_f.flush()
-            self._video_f.close()
-            self._video_f = None
-
-    def flush(self, flush_video_file: bool = True):
-        if self._batch_items:
-            if len(self._batch_items[0].shape) == 3:
-                image_batch = torch.stack(self._batch_items)
-            else:
-                image_batch = torch.cat(self._batch_items, dim=0)
-            self._batch_items.clear()
-            #image_batch = self.bgr_to_rgb(batch=image_batch)
-            frame_count = len(image_batch)
-            self._video_out.write_video_chunk(
-                i=0,
-                chunk=image_batch,
-            )
-            self._frame_counter += frame_count
-
-            # w = torchaudio.io.StreamWriter("badvid.mp4", format="mp4")
-            # w.add_video_stream(
-            #     frame_rate=self._fps,
-            #     height=self._height,
-            #     width=self._width,
-            #     format="bgr24",
-            #     encoder="hevc_nvenc",
-            #     encoder_format="bgr0",
-            #     hw_accel="cuda:0",
-            # )
-            # with w.open():
-            #     w.write_video_chunk(0, image_batch.to(torch.device("cuda:0")))
-
-        if flush_video_file and self._video_f is not None:
-            self._video_f.flush()
-
-    def isOpened(self):
-        return self._video_f is not None
-
-    def open(self):
-        assert self._video_f is None
-        self._video_out = torchaudio.io.StreamWriter(dst=self._filename + ".mp4", format="mp4")
-        self._add_stream()
-        self._video_f = self._video_out.open()
-
-    def set(self, key: int, value: any):
-        pass
-
-    def get(self, key: int) -> any:
-        return None
-
-    def append(self, images: torch.Tensor):
-        self._batch_items.append(self._make_proper_permute(images))
-        if len(self._batch_items) >= self._batch_size:
-            self.flush(flush_video_file=False)
 
 
 def main(args):
---
+++ src/hmlib/video_stream.py
@@ -0,0 +1,131 @@
+import torch
+import torchaudio
+
+
+class VideoStreamWriter:
+    def __init__(
+        self,
+        filename: str,
+        fps: float,
+        width: int,
+        height: int,
+        codec: str,
+        format: str = "bgr24",
+        batch_size: int = 10,
+        bit_rate: int = 44000,
+        device: torch.device = None,
+        lossless: bool = False,
+    ):
+        self._filename = filename
+        self._fps = fps
+        self._width = width
+        self._height = height
+        self._codec = codec
+        self._format = format
+        self._video_out = None
+        self._video_f = None
+        self._device = device
+        self._lossless = lossless
+        assert batch_size >= 1
+        self._batch_size = batch_size
+        self._batch_items = []
+        self._in_flush = False
+        self._codec_config = torchaudio.io.CodecConfig(
+            bit_rate=int(5e6),
+        )
+        self._codec_config.bit_rate = bit_rate
+        self._codec_config = None
+        self._frame_counter = 0
+
+    def __enter__(self):
+        if self._video_f is None:
+            self.open()
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.close()
+
+    def _make_proper_permute(self, image: torch.Tensor):
+        if len(image.shape) == 3:
+            if image.shape[-1] == 3 and self._device is not None:
+                image = image.permute(2, 0, 1)
+        else:
+            if image.shape[-1] == 3 and self._device is not None:
+                image = image.permute(0, 3, 1, 2)
+        return image
+
+    def _add_stream(self):
+        # encoder="h264_nvenc", encoder_format="yuv444p", hw_accel="cuda:0"
+        if self._lossless:
+            preset = "losslesshp"
+            #preset = "lossless"
+            rate_control = "constqp"
+        else:
+            preset = "slow"
+            rate_control = "cbr"
+        self._video_out.add_video_stream(
+            frame_rate=self._fps,
+            height=self._height,
+            width=self._width,
+            format=self._format,
+            encoder=self._codec,
+            encoder_format="bgr0",
+            encoder_option={
+                "preset": preset,
+                "rc": rate_control,
+            },
+            codec_config=self._codec_config,
+            hw_accel=str(self._device),
+        )
+        print("Video stream added")
+
+    def bgr_to_rgb(self, batch: torch.Tensor):
+        # Assuming batch is a PyTorch tensor of shape [N, C, H, W]
+        # and the channel order is BGR
+        return batch[:, [2, 1, 0], :, :]
+
+    def close(self):
+        # self.flush()
+        if self._video_f is not None:
+            self._video_f.close()
+            self._video_f = None
+
+    def flush(self, flush_video_file: bool = True):
+        if self._batch_items:
+            if len(self._batch_items[0].shape) == 3:
+                image_batch = torch.stack(self._batch_items)
+            else:
+                image_batch = torch.cat(self._batch_items, dim=0)
+            self._batch_items.clear()
+            #image_batch = self.bgr_to_rgb(batch=image_batch)
+            frame_count = len(image_batch)
+            self._video_out.write_video_chunk(
+                i=0,
+                chunk=image_batch,
+            )
+            self._frame_counter += frame_count
+
+        if flush_video_file and self._video_f is not None:
+            self._video_f.flush()
+
+    def isOpened(self):
+        return self._video_f is not None
+
+    def open(self):
+        assert self._video_f is None
+        self._video_out = torchaudio.io.StreamWriter(dst=self._filename + ".mp4", format="mp4")
+        self._add_stream()
+        self._video_f = self._video_out.open()
+
+    def set(self, key: int, value: any):
+        pass
+
+    def get(self, key: int) -> any:
+        return None
+
+    def append(self, images: torch.Tensor):
+        self._batch_items.append(self._make_proper_permute(images))
+        if len(self._batch_items) >= self._batch_size:
+            self.flush(flush_video_file=False)
+
+
