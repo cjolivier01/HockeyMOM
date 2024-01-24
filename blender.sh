b69ea2a323ce8897d00fb9145ebb640529174762
Christopher Olivier <cjolivier01@gmail.com>
2024-01-23 17:55:07 -0800

    ..

--- blender.sh
+++ blender.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-#VIDEO_DIR="/home/colivier-local/Videos/sharksbb1-2"
-#VIDEO_DIR="/home/colivier-local/Videos/onehockey-sharksbb2"
-VIDEO_DIR="/home/colivier-local/Videos/onehockey-sharks10a"
-#OFFSETS="--lfo=54.70108123169348 --rfo=0"
-OFFSETS="--lfo=0 --rfo=0.627263892570015"
-ROTATION="--rotation_angle=-25"
+VIDEO_DIR="/home/colivier-local/Videos/sharks-bb3-2"
+#ROTATION="--rotation_angle=-25"
 
 OMP_NUM_THREADS=16 \
 	PYTHONPATH=$(pwd):$(pwd)/src:$(pwd)/models/mixsort \
