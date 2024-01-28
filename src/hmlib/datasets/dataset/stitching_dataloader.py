b702d030016ca1b1a720de8b4dcec0265bc970e1
Christopher Olivier <cjolivier01@gmail.com>
2024-01-28 14:46:50 -0800

    HM_NO_CUDA_STITCH_ENCODER

--- src/hmlib/datasets/dataset/stitching_dataloader.py
+++ src/hmlib/datasets/dataset/stitching_dataloader.py
@@ -55,6 +55,8 @@ def distribute_items_detailed(total_item_count, worker_count):
 
 def _get_cuda_device():
     # Use last device
+    if int(os.environ.get("HM_NO_CUDA_STITCH_ENCODER", "0")):
+        return torch.device("cpu")
     return torch.device("cuda", torch.cuda.device_count() - 1)
 
 
