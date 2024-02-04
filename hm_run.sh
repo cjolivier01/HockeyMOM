f644f4103261c99d14a373be6651c6310d56f1e5
Christopher Olivier <cjolivier01@gmail.com>
2024-02-04 15:46:11 -0800

    ..

--- hm_run.sh
+++ hm_run.sh
@@ -3,18 +3,10 @@
 EXPERIMENT_FILE="models/mixsort/exps/example/mot/yolox_x_ch.py"
 
 #
-# Models
-#
-#PRETRAINED_MODEL="-c=./pretrained/dla34/my_coco_ch_e60.pth"
-#PRETRAINED_MODEL="-c=./pretrained/dla34/crowdhuman_dla34.pth"
-
-MIXFORMER_SCRIPT="mixformer_deit_hockey"
-
-#
 # Videos
 #
 #VIDEO="${HOME}/Videos/lbd2/stitched_output-with-audio.avi"
-#VIDEO="${HOME}/Videos/tvbb-1"
+#VIDEO="${HOME}/Videos/sharks-bb1-2"
 
 #GAME_ID="tvbb2"
 GAME_ID="sharks-bb1-2"
@@ -22,16 +14,17 @@ GAME_ID="sharks-bb1-2"
 
 TEST_SIZE_ARG="--test-size=300x780"
 
-BLEND_MODE="--blend-mode=laplacian"
-#BLEND_MODE="--blend-mode=gpu-hard-seam"
+#BLEND_MODE="--blend-mode=laplacian"
+BLEND_MODE="--blend-mode=gpu-hard-seam"
 
 START_FRAME=0
 #START_FRAME=1900
 #START_FRAME=2900
 #START_FRAME=8730
+START_FRAME=30000
 #START_FRAME=47000
 #START_FRAME=16890
-START_FRAME=25000
+#START_FRAME=50000
 #START_FRAME=46200
 
 #TRACKER="--tracker=hm"
@@ -39,8 +32,8 @@ START_FRAME=25000
 #BATCH_SIZE=32
 #BATCH_SIZE=16
 #BATCH_SIZE=16
-BATCH_SIZE=6
-#BATCH_SIZE=8
+#BATCH_SIZE=2
+BATCH_SIZE=8
 
 #STITCHING_PARAMS="--lfo=42.63559569682018 --rfo=0"
 
@@ -60,7 +53,6 @@ OMP_NUM_THREADS=16 \
   python src/hmtrack.py \
   -expn="${GAME_ID}" \
   -f="${EXPERIMENT_FILE}" \
-  --script="${MIXFORMER_SCRIPT}" \
   ${PRETRAINED_MODEL} \
   -b=${BATCH_SIZE} \
   --gpus=0,1,2 \
