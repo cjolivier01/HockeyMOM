90316f2e7b7fce8d0daafd31561c507f4ce03b45
Christopher Olivier <cjolivier01@gmail.com>
2024-02-04 11:32:37 -0800

    ..

--- hockeymom/csrc/pytorch/image_blend.cpp
+++ hockeymom/csrc/pytorch/image_blend.cpp
@@ -322,10 +322,10 @@ std::pair<at::Tensor, at::Tensor> ImageBlender::make_full(
     x2 = 0;
   }
 
-  std::cout << "Canvas size=[" << canvas_h << ", " << canvas_w << "]"
-            << std::endl;
-  std::cout << "image_1 size=" << image_1.sizes()
-            << "\nimage_2 size=" << image_2.sizes() << std::endl;
+  // std::cout << "Canvas size=[" << canvas_h << ", " << canvas_w << "]"
+  //           << std::endl;
+  // std::cout << "image_1 size=" << image_1.sizes()
+  //           << "\nimage_2 size=" << image_2.sizes() << std::endl;
 
   TORCH_CHECK(x1 == 0 || x2 == 0, "Images not aligned to left edge of canvas");
   TORCH_CHECK(y1 == 0 || y2 == 0, "Images not aligned to top edge of canvas");
