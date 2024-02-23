bcaa43e39194df77575125b4aa3d68fad990d754
Christopher Olivier <cjolivier01@gmail.com>
2024-02-22 19:34:33 -0800

    ..

--- stitch.sh
+++ stitch.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-#GAME_ID="sharks-bb2-2"
-#GAME_ID="sharksbb1-1"
-#GAME_ID="tvbb"
-#GAME_ID="nm-wolves"
-
-VIDEO_DIR="${HOME}/Videos/${GAME_ID}"
-
 OMP_NUM_THREADS=24 \
 	PYTHONPATH=$(pwd):$(pwd)/models/mixsort \
-	python src/stitch.py --video_dir="${VIDEO_DIR}" --game-id=${GAME_ID} --project_file=autooptimiser_out.pto ${OFFSETS}  $@
+	python src/stitch.py --game-id=${GAME_ID} --project_file=autooptimiser_out.pto ${OFFSETS}  $@
