ba032b51abc6053fd818eb8a1db975f97de773d9
Chris Olivier <cjolivier01@gmail.comr>
2024-09-17 16:37:50 -0700

    cleanup

--- asan.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ASAN_OPTIONS=symbolize=1 \
-  LD_PRELOAD=$(clang -print-file-name=libclang_rt.asan-x86_64.so) \
-  ASAN_SYMBOLIZER_PATH="$(which llvm-symbolizer)" \
-  OMP_NUM_THREADS=16 \
-  PYTHONPATH=$(pwd):$(pwd)/models/mixsort \
-  $@
