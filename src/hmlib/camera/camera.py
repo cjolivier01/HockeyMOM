b71d740ce86109144830117c56781419e5e9c0ca
Chris Olivier <cjolivier01@gmail.comr>
2024-10-05 14:19:13 -0700

    removing obsolete stuff

--- src/hmlib/camera/camera.py
+++ src/hmlib/camera/camera.py
@@ -131,9 +131,9 @@ class TlwhHistory(object):
         return self._current_image_speed
 
 
-class Clustering:
-    def __init__(self):
-        pass
+# class Clustering:
+#     def __init__(self):
+#         pass
 
 
 CAMERA_TYPE_MAX_SPEEDS = {
@@ -153,7 +153,7 @@ class HockeyMOM:
         image_width: int,
         image_height: int,
         fps: float,
-        device,
+        device: torch.device,
         camera_name: str,
         max_history: int = 26,
         speed_history: int = 26,
@@ -163,7 +163,6 @@ class HockeyMOM:
             image_width=self._to_scalar_float(image_width),
             image_height=self._to_scalar_float(image_height),
         )
-        self._clamp_box = self._video_frame.bounding_box()
         self._online_tlwhs_history: List[torch.Tensor] = list()
         self._max_history = max_history
         self._online_ids: Set[int] = set()
@@ -178,22 +177,11 @@ class HockeyMOM:
 
         self._epsilon = self._to_scalar_float(0.00001)
 
-        #
-        # The camera's box speed itself
-        #
-        # self._current_camera_box_speed_x = self._to_scalar_float(0)
-        # self._current_camera_box_speed_y = self._to_scalar_float(0)
-
-        # self._current_camera_box_speed_reversed_x = False
-        # self._current_camera_box_speed_reversed_y = False
-
-        self._camera_box_max_speed_x = (
-            self._to_scalar_float(max(image_width / CAMERA_TYPE_MAX_SPEEDS[camera_name], 12.0))
-            / self._fps_speed_scale
+        self._camera_box_max_speed_x = self._to_scalar_float(
+            max(image_width / CAMERA_TYPE_MAX_SPEEDS[camera_name], 12.0)
         )
-        self._camera_box_max_speed_y = (
-            self._to_scalar_float(max(image_height / CAMERA_TYPE_MAX_SPEEDS[camera_name], 12.0))
-            / self._fps_speed_scale
+        self._camera_box_max_speed_y = self._to_scalar_float(
+            max(image_height / CAMERA_TYPE_MAX_SPEEDS[camera_name], 12.0)
         )
         logger.info(
             f"Camera Max speeds: x={self._camera_box_max_speed_x}, y={self._camera_box_max_speed_y}"
@@ -205,24 +193,8 @@ class HockeyMOM:
             f"Camera Max acceleration: dx={self._camera_box_max_accel_x}, dy={self._camera_box_max_accel_y}"
         )
 
-        # self._last_acceleration_dx = self._to_scalar_float(0)
-        # self._last_acceleration_dy = self._to_scalar_float(0)
-
-        #
-        # Zoom velocity
-        #
-        # self._camera_box_size_change_velocity_x = self._to_scalar_float(0)
-        # self._camera_box_size_change_velocity_y = self._to_scalar_float(0)
-
-        # self._camera_box_max_size_change_velocity_x = self._to_scalar_float(2)
-        # self._camera_box_max_size_change_velocity_y = self._to_scalar_float(2)
-
     def _to_scalar_float(self, scalar_float):
         return torch.tensor(scalar_float, dtype=torch.float, device=self._device)
-
-    # def is_fast(self, speed: float = 7):
-    #     # return abs(self._current_camera_box_speed_x) > speed or abs(self._current_camera_box_speed_y) > speed
-    #     return self.get_speed() >= speed
 
     def append_online_objects(self, online_ids, online_tlws):
         # assert isinstance(online_tlwh_map, dict)
@@ -250,70 +222,12 @@ class HockeyMOM:
             self._id_to_tlwhs_history_map[id.item()] = hist
 
     @property
-    def online_image_center_points(self):
-        return self._online_image_center_points
-
-    @property
     def video(self):
         return self._video_frame
 
-    @property
-    def clamp_box(self):
-        return self._clamp_box
-
-    def get_image_tracking(self, online_ids):
-        results = []
-        for id in online_ids:
-            results.append(self._id_to_tlwhs_history_map[id.item()].image_position_history)
-        return results
-
-    def get_fast_ids(self, min_fast_items: int = 4, max_fast_items: int = 6):
-        return self._prune_slow_ids(
-            id_to_pos_history=self._id_to_tlwhs_history_map,
-            min_fast_items=min_fast_items,
-            max_fast_items=max_fast_items,
-        )
-
-    # def get_spatial_speed(self, id: int):
-    #     return self._id_to_tlwhs_history_map[id].spatial_speed
-
-    def get_image_speed(self, id: int):
-        return self._id_to_tlwhs_history_map[id.item()].image_speed
-
-    def get_tlwh(self, id: int):
+    def get_tlwh(self, id: int) -> torch.Tensor:
         position_history = self._id_to_tlwhs_history_map[id.item()]
         return position_history.image_position_history[-1]
-
-    @staticmethod
-    def _prune_slow_ids(
-        id_to_pos_history: Dict[int, TlwhHistory],
-        fast_threshhold: float = 0.5,
-        min_fast_items: int = -1,
-        max_fast_items: int = -1,
-        min_history_length: int = 20,
-    ) -> List[int]:
-        # Sort by speed
-        keys = list(id_to_pos_history.keys())
-        # speeds = [hist.spatial_speed for hist in id_to_pos_history.values()]
-        speeds = [hist.image_speed for hist in id_to_pos_history.values()]
-        sorted_value_index = np.argsort(speeds)
-        sorted_ids = reversed([keys[i] for i in sorted_value_index])
-        sorted_speeds = reversed([speeds[i] for i in sorted_value_index])
-        fast_ids = []
-        # trunk-ignore(ruff/B905)
-        for sorted_id, sorted_speed in zip(sorted_ids, sorted_speeds):
-            if sorted_speed >= fast_threshhold:
-                if len(id_to_pos_history[sorted_id]) >= min_history_length:
-                    fast_ids.append(sorted_id)
-                    if max_fast_items > 0 and len(fast_ids) >= max_fast_items:
-                        break
-
-        if min_fast_items > 0:
-            if len(fast_ids) < min_fast_items:
-                fast_ids = []
-
-        # Now prune to
-        return fast_ids
 
     def get_group_x_velocity(
         self, min_considered_velocity: float = 0.01, group_threshhold: float = 0.6
@@ -332,7 +246,7 @@ class HockeyMOM:
                 leftmost_center = this_pos_center
             if rightmost_center is None:
                 rightmost_center = this_pos_center
-            image_speed = hist.current_image_x_speed
+            image_speed = hist.current_image_x_speed * self._fps_speed_scale
             if image_speed < -min_considered_velocity:
                 neg_count += 1
                 neg_sum += image_speed
@@ -355,66 +269,6 @@ class HockeyMOM:
                 return avg_x_speed, leftmost_center
         return 0, None
 
-    # def add_x_velocity(self, x_velocity_to_add):
-    #     self._current_camera_box_speed_x += x_velocity_to_add
-
-    def clamp(self, box: torch.Tensor):
-        return clamp_box(box, self._video_frame.bounding_box())
-
-    @staticmethod
-    def _tlwh_to_tlbr(box):
-        return torch.tensor(box[0], box[1], box[0] + box[2], box[1] + box[3])
-
-    @staticmethod
-    def _tlbr_to_tlwh(box):
-        return torch.tensor(box[0], box[1], box[2] - box[0], box[3] - box[1])
-
-    @staticmethod
-    def _union(box1, box2):
-        box = HockeyMOM._tlwh_to_tlbr(box1)
-        box2 = HockeyMOM._tlwh_to_tlbr(box2)
-        box[0] = min(box[0], box2[0])
-        box[1] = min(box[1], box2[1])
-        box[2] = max(box[2], box2[2])
-        box[3] = max(box[3], box2[3])
-        return HockeyMOM._tlbr_to_tlwh(box1)
-
-    @classmethod
-    def union_box(cls, box1, box2):
-        assert box1.dtype == box2.dtype
-        top_left = torch.min(box1[:2], box2[:2])
-        bottom_right = torch.max(box1[2:], box2[2:])
-        return torch.cat([top_left, bottom_right])
-
-    @staticmethod
-    def _make_pruned_map(map, allowed_keys):
-        key_set = set(allowed_keys)
-        new_map = dict()
-        for map_key in map.keys():
-            if map_key in key_set:
-                new_map[map_key] = map[map_key]
-        return new_map
-
-    @staticmethod
-    def _normalize_map(map, reference_map):
-        for key in reference_map:
-            if key not in map:
-                map[key] = reference_map[key]
-        return map
-
-    @classmethod
-    def box_str(cls, box):
-        box_center = center(box)
-        return (
-            f"[x1={box[0]}, y1={box[1]}, x2={box[2]}, y2={box[3]}] "
-            + f"w={box[2] - box[0]}, h={box[3] - box[1]}, "
-            + f"center={box_center}"
-        )
-
-    @classmethod
-    def print_box(cls, box):
-        logger.info(cls.box_str(box))
-
     def get_current_bounding_box(self, ids=None):
         if ids is None:
             ids = self._online_ids
@@ -432,38 +286,3 @@ class HockeyMOM:
         # The containing bounding box is then
         containing_box = torch.cat([min_tl, max_br])
         return containing_box
-
-    def shift_box_to_edge(self, box, strict: bool = False):
-        """
-        If a box is off the edge of the image, translate
-        the box to be flush with the edge instead.
-        """
-        if strict:
-            assert width(box) <= self._video_frame.width
-            assert height(box) <= self._video_frame.height
-        else:
-            if width(box) > self._video_frame.width:
-                logger.info(
-                    f"ERROR: Width {width(box)} is too wide! Larger than video frame width of {self._video_frame.width}"
-                )
-            if height(box) > self._video_frame.height:
-                logger.info(
-                    f"ERROR: Height {height(box)} is too tall! Larger than video frame width of {self._video_frame.height}"
-                )
-
-        if box[0] < 0:
-            box[2] += -box[0]
-            box[0] += -box[0]
-        elif box[2] >= self._video_frame.width:
-            offset = box[2] - (self._video_frame.width - 1)
-            box[0] -= offset
-            box[2] -= offset
-
-        if box[1] < 0:
-            box[3] += -box[1]
-            box[1] += -box[1]
-        elif box[3] >= self._video_frame.height:
-            offset = box[3] - (self._video_frame.height - 1)
-            box[1] -= offset
-            box[3] -= offset
-        return box
