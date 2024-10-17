d44ef8541988a5a7c591082391888827b1a92c7b
Christopher Olivier <colivier@tesla.com>
2024-10-17 17:19:26 +0000

    ..

--- env/run.sh
+++ env/run.sh
@@ -2,4 +2,5 @@
 docker run --gpus all --privileged -it --user=$(id -u):$(id -g) \
   -v /home/$USER:/home/$USER \
   -v /mnt/home/$USER:/mnt/home/$USER \
+  -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v /etc/shadow:/etc/shadow:ro -v /etc/gshadow:/etc/gshadow:ro -v /etc/sudoers:/etc/sudoers:ro \
   --workdir=/home/$USER $@ hm
