3036e4bf4bcd7cb406ce70086da2177a3b526d19
Christopher Olivier <colivier@tesla.com>
2024-10-17 14:19:42 -0700

    ..

--- env/run.sh
+++ env/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --gpus all --privileged --user=$(id -u):$(id -g) -it \
-  --memory 40g \
+  --memory 32g \
   -v /home/$USER:/home/$USER \
   -v /mnt/home/$USER:/mnt/home/$USER \
   -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v /etc/shadow:/etc/shadow:ro -v /etc/gshadow:/etc/gshadow:ro -v /etc/sudoers:/etc/sudoers:ro \
