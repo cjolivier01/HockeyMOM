12e02cfa61dfe446d0320ba24cc3ace41f375af7
Christopher Olivier <colivier@tesla.com>
2024-10-17 10:27:41 -0700

    ...

--- env/rund.sh
+++ env/rund.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-docker run --gpus all --privileged --user=$(id -u):$(id -g) -it \
-  -v /home/$USER:/home/$USER \
-  -v /mnt/home/$USER:/mnt/home/$USER \
+docker run --gpus all --privileged --user=$(id -u):$(id -g) -d \
+  -v /home:/home \
+  -v /mnt/home:/mnt/home \
   -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v /etc/shadow:/etc/shadow:ro -v /etc/gshadow:/etc/gshadow:ro -v /etc/sudoers:/etc/sudoers:ro \
+  --name hockeymom \
   --workdir=/home/$USER $@ hm
