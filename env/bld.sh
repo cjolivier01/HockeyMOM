5c0d887651189226eb0fc31aa7a3d204092244bb
Christopher Olivier <cjolivier01@gmail.com>
2024-10-18 12:06:13 -0700

    ...

--- env/bld.sh
+++ env/bld.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --memory 45G  --build-arg USERNAME=$(whoami) --build-arg UID=$(id -u) --build-arg GID=$(id -g) . -t hm
+docker build --memory 45G  --build-arg USERNAME=$(whoami) --build-arg UID=$(id -u) --build-arg GID=$(id -g) $@ .
--- env/run.sh
+++ env/run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+# -v /mnt/home/$USER:/mnt/home/$USER
+
 docker run --gpus all --privileged --user=$(id -u):$(id -g) -it \
   --memory 32g \
-  -v /home/$USER:/home/$USER \
-  -v /mnt/home/$USER:/mnt/home/$USER \
-  -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v /etc/shadow:/etc/shadow:ro -v /etc/gshadow:/etc/gshadow:ro -v /etc/sudoers:/etc/sudoers:ro \
-  --workdir=/home/$USER $@ hm
+  -v ${HOME}:${HOME} \
+  -v ${HOME}/.ssh:${HOME}/.ssh \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /etc/group:/etc/group:ro \
+  -v /etc/shadow:/etc/shadow:ro \
+  -v /etc/gshadow:/etc/gshadow:ro \
+  -v /etc/sudoers:/etc/sudoers:ro \
+  --workdir=${HOME} $@
