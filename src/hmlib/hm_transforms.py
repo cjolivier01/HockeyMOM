78217b008e5e50172caca0f7460bfda3ea8d7c96
Chris Olivier <cjolivier01@gmail.comr>
2024-10-23 19:24:11 -0700

    fix

--- src/hmlib/hm_transforms.py
+++ src/hmlib/hm_transforms.py
@@ -1334,122 +1334,6 @@ class HmTopDownGetBboxCenterScale:
         return results
 
 
-# @TRANSFORMS.register_module()
-# class HmVideoCollect(object):
-#     """Collect data from the loader relevant to the specific task.
-
-#     Args:
-#         keys (Sequence[str]): Keys of results to be collected in ``data``.
-#         meta_keys (Sequence[str]): Meta keys to be converted to
-#             ``mmcv.DataContainer`` and collected in ``data[img_metas]``.
-#             Defaults to None.
-#         default_meta_keys (tuple): Default meta keys. Defaults to ('filename',
-#             'ori_filename', 'ori_shape', 'img_shape', 'pad_shape',
-#             'scale_factor', 'flip', 'flip_direction', 'img_norm_cfg',
-#             'frame_id', 'is_video_data').
-#     """
-
-#     def __init__(
-#         self,
-#         keys,
-#         meta_keys=None,
-#         default_meta_keys=(
-#             "filename",
-#             "ori_filename",
-#             "ori_shape",
-#             "img_shape",
-#             "pad_shape",
-#             "scale_factor",
-#             "flip",
-#             "flip_direction",
-#             "img_norm_cfg",
-#             "frame_id",
-#             "is_video_data",
-#         ),
-#     ):
-#         self.keys = keys
-#         self.meta_keys = default_meta_keys
-#         if meta_keys is not None:
-#             if isinstance(meta_keys, str):
-#                 meta_keys = (meta_keys,)
-#             else:
-#                 assert isinstance(meta_keys, tuple), "meta_keys must be str or tuple"
-#             self.meta_keys += meta_keys
-
-#     def __call__(self, results):
-#         """Call function to collect keys in results.
-
-#         The keys in ``meta_keys`` and ``default_meta_keys`` will be converted
-#         to :obj:mmcv.DataContainer.
-
-#         Args:
-#             results (list[dict] | dict): List of dict or dict which contains
-#                 the data to collect.
-
-#         Returns:
-#             list[dict] | dict: List of dict or dict that contains the
-#             following keys:
-
-#             - keys in ``self.keys``
-#             - ``img_metas``
-#         """
-#         results_is_dict = isinstance(results, dict)
-#         if results_is_dict:
-#             results = [results]
-#         outs = []
-#         for _results in results:
-#             _results = self._add_default_meta_keys(_results)
-#             _results = self._collect_meta_keys(_results)
-#             outs.append(_results)
-
-#         # if results_is_dict:
-#         #     outs[0]["img_metas"] = DC(outs[0]["img_metas"], cpu_only=True)
-
-#         return outs[0] if results_is_dict else outs
-
-#     def _collect_meta_keys(self, results):
-#         """Collect `self.keys` and `self.meta_keys` from `results` (dict)."""
-#         data = {}
-#         img_meta = {}
-#         for key in self.meta_keys:
-#             if key in results:
-#                 img_meta[key] = results[key]
-#             elif key in results["img_info"]:
-#                 img_meta[key] = results["img_info"][key]
-#         data["img_metas"] = img_meta
-#         for key in self.keys:
-#             if key in results:
-#                 data[key] = results[key]
-#         return data
-
-#     def _add_default_meta_keys(self, results):
-#         """Add default meta keys.
-
-#         We set default meta keys including `pad_shape`, `scale_factor` and
-#         `img_norm_cfg` to avoid the case where no `Resize`, `Normalize` and
-#         `Pad` are implemented during the whole pipeline.
-
-#         Args:
-#             results (dict): Result dict contains the data to convert.
-
-#         Returns:
-#             results (dict): Updated result dict contains the data to convert.
-#         """
-#         img = results["img"]
-#         results.setdefault("pad_shape", img.shape)
-#         results.setdefault("scale_factor", 1.0)
-#         num_channels = 1 if len(img.shape) < 3 else img.shape[2]
-#         results.setdefault(
-#             "img_norm_cfg",
-#             dict(
-#                 mean=np.zeros(num_channels, dtype=np.float32),
-#                 std=np.ones(num_channels, dtype=np.float32),
-#                 to_rgb=False,
-#             ),
-#         )
-#         return results
-
-
 def _to_float(t: Union[np.ndarray, torch.Tensor]):
     if isinstance(t, torch.Tensor):
         if not torch.is_floating_point(t):
@@ -1489,5 +1373,5 @@ class HmLoadImageFromWebcam(LoadImageFromFile):
         results["ori_filename"] = [None for _ in range(batch_size)]
         results["img_shape"] = [shape for _ in range(batch_size)]
         results["ori_shape"] = [shape for _ in range(batch_size)]
-        results["img_fields"] = ["img" for _ in range(batch_size)]
+        results["img_fields"] = ["img"]
         return results
