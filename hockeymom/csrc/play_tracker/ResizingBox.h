eb712d8b89f31bfb222737243ff59a5ed73a336d
Chris Olivier <cjolivier01@gmail.comr>
2024-12-07 05:51:12 -0800

    ...

--- hockeymom/csrc/play_tracker/ResizingBox.h
+++ hockeymom/csrc/play_tracker/ResizingBox.h
@@ -18,193 +18,43 @@ struct ResizingState {
 class ResizingBox : virtual public IBasicLivingBox {
  public:
   ResizingBox(ResizingBox&&) = delete;
-  ResizingBox(const ResizingConfig& config) : config_(config) {}
+  ResizingBox(const ResizingConfig& config);
 
-  void set_destination(const BBox& dest_box) override {
-    if (!config_.resizing_enabled) {
-      return;
-    }
-    set_destination_size(dest_box.width(), dest_box.height());
-  }
+  void set_destination(const BBox& dest_box) override;
 
-  const ResizingConfig& get_config() const {
-    return config_;
-  }
-
-  const ResizingState& get_state() const {
-    return state_;
-  }
+  const ResizingState& get_state() const;
 
  protected:
-  SizeDiff get_proposed_next_size_change() const {
-    if (state_.size_is_frozen) {
-      return SizeDiff{.dw = zero(), .dh = zero()};
-    }
-    return SizeDiff{
-        .dw = state_.current_speed_w / 2, .dh = state_.current_speed_h / 2};
-  }
+  const ResizingConfig& get_config() const;
 
-  WHDims get_min_allowed_width_height() const {
-    return WHDims{.width = config_.min_width, .height = config_.min_height};
-  }
+  SizeDiff get_proposed_next_size_change() const;
 
-  // WHDims get_max_allowed_width_height() const {
-  //   return WHDims{.width = config_.max_width, .height = config_.max_height};
-  // }
+  WHDims get_min_allowed_width_height() const;
 
-  void clamp_size_scaled() {
-    const BBox bbox = bounding_box();
-    float w = bbox.width();
-    float h = bbox.height();
-    float wscale = zero(), hscale = zero();
-    if (w > config_.max_width) {
-      wscale = config_.max_width / w;
-    }
-    if (h > config_.max_height) {
-      hscale = config_.max_height / h;
-    }
-    float final_scale = std::max(wscale, hscale);
-    if (!isZero(final_scale)) {
-      w *= final_scale;
-      h *= final_scale;
-      set_bbox(BBox(bbox.center(), WHDims{.width = w, .height = h}));
-    }
-  }
+  void clamp_size_scaled();
 
  private:
   void set_destination_size(
       FloatValue dest_width,
       FloatValue dest_height,
-      bool prioritize_width_thresh = true) {
-    BBox bbox = bounding_box();
-    auto current_w = bbox.width();
-    auto current_h = bbox.height();
-
-    auto dw = dest_width - current_w;
-    auto dh = dest_height - current_h;
-
-    if (config_.sticky_sizing) {
-      //
-      // Begin size threshholding stuff
-      //
-      GrowShrink resize_rates = get_grow_shrink_wh(bbox);
-
-      // dw
-      bool dw_thresh = dw < 0 && dw < -resize_rates.shrink_width;
-      const bool want_bigger_w = dw > 0 && dw > resize_rates.grow_width;
-      dw_thresh |= want_bigger_w;
-      if (!dw_thresh) {
-        dw = 0.0f;
-      }
-
-      // dh
-      bool dh_thresh = dh < 0 && dh < -resize_rates.shring_height;
-      const bool want_bigger_h = dh > 0 && dh > resize_rates.grow_height;
-      dh_thresh |= want_bigger_h;
-      if (!dh_thresh) {
-        dh = 0.0f;
-      }
-
-      bool both_thresh = dw_thresh && dh_thresh;
-      if (prioritize_width_thresh) {
-        both_thresh |= dw_thresh;
-      }
-      const bool any_thresh = dw_thresh || dh_thresh;
-      const bool want_bigger = (want_bigger_w || want_bigger_h) && any_thresh;
-      state_.size_is_frozen |= !(both_thresh || want_bigger);
-      //
-      // End size threshholding stuff
-      //
-    }
-
-    constexpr FloatValue kMaxWidthHeightDiffDirectionAssumeStoppedMaxRatio =
-        6.0;
-    constexpr FloatValue kMaxWidthHeightDiffDirectionCutRateRatio = 2.0;
-    if (different_directions(dw, state_.current_speed_w)) {
-      // The desired change is in the opposire direction of the current widening
-      if (std::abs(state_.current_speed_w) <
-          (config_.max_speed_w /
-           kMaxWidthHeightDiffDirectionAssumeStoppedMaxRatio)) {
-        // It's small enough, so just stop the velocity in the opposite
-        // direction of the desired change
-        state_.current_speed_w = 0;
-      } else {
-        state_.current_speed_w /= kMaxWidthHeightDiffDirectionCutRateRatio;
-      }
-    }
-    if (different_directions(dh, state_.current_speed_h)) {
-      // The desired change is in the opposire direction of the current
-      // heightening
-      if (std::abs(state_.current_speed_h) <
-          (config_.max_speed_h /
-           kMaxWidthHeightDiffDirectionAssumeStoppedMaxRatio)) {
-        // It's small enough, so just stop the velocity in the opposite
-        // direction of the desired change
-        state_.current_speed_h = 0;
-      } else {
-        state_.current_speed_h /= kMaxWidthHeightDiffDirectionCutRateRatio;
-      }
-    }
-
-    adjust_size(/*accel_w=*/dw, /*accel_h=*/dh);
-  }
+      bool prioritize_width_thresh = true);
 
   void adjust_size(
       FloatValue accel_w,
       FloatValue accel_h,
-      bool use_constraints = true) {
-    if (state_.size_is_frozen) {
-      return;
-    }
+      bool use_constraints = true);
 
-    if (use_constraints) {
-      constexpr FloatValue kResizeLargerScaleDifference = 2.0;
-
-      // Growing is allowed at a higher rate than shrinking
-      const FloatValue max_accel_w = accel_w > 0
-          ? (config_.max_accel_w * kResizeLargerScaleDifference)
-          : config_.max_accel_w;
-      const FloatValue max_accel_h = accel_h > 0
-          ? (config_.max_accel_h * kResizeLargerScaleDifference)
-          : config_.max_accel_h;
-
-      accel_w = clamp(accel_w, -max_accel_w, max_accel_w);
-      accel_h = clamp(accel_h, -max_accel_h, max_accel_h);
-    }
-
-    state_.current_speed_w += accel_w;
-    state_.current_speed_h += accel_h;
-
-    if (use_constraints) {
-      clamp_resizing();
-    }
-  }
-
-  void clamp_resizing() {
-    state_.current_speed_w = clamp(
-        state_.current_speed_w, -config_.max_speed_w, config_.max_speed_w);
-    state_.current_speed_h = clamp(
-        state_.current_speed_h, -config_.max_speed_h, config_.max_speed_h);
-  }
+  void clamp_resizing();
 
   struct GrowShrink {
     FloatValue grow_width, grow_height, shrink_width, shring_height;
   };
 
-  GrowShrink get_grow_shrink_wh(const BBox& bbox) const {
-    auto ww = bbox.width(), hh = bbox.height();
-    return GrowShrink{
-        .grow_width = ww * config_.size_ratio_thresh_grow_dw,
-        .grow_height = hh * config_.size_ratio_thresh_grow_dh,
-        .shrink_width = ww * config_.size_ratio_thresh_shrink_dw,
-        .shring_height = hh * config_.size_ratio_thresh_shrink_dh,
-    };
-  }
+  GrowShrink get_grow_shrink_wh(const BBox& bbox) const;
 
   const ResizingConfig config_;
   ResizingState state_;
 };
-
 
 } // namespace play_tracker
 } // namespace hm
