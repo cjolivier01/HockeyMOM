dec8bf0346ee1559639952159d0f87300147fda5
Chris Olivier <cjolivier01@gmail.comr>
2024-12-07 05:19:09 -0800

    ...

--- src/hmlib/camera/living_box.py
+++ src/hmlib/camera/living_box.py
@@ -2,9 +2,20 @@ import torch
 
 from hockeymom.core import AllLivingBoxConfig, BBox, LivingBox
 
+
 class PyLivingBox(LivingBox):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-    
-    def draw(self, torch.Tensor) -> torch.Tensor:
-        pass
+
+    def _draw_resizing_state(self, img: torch.Tensor):
+        rstate = self.resizing_state()
+        return img
+
+    def _draw_translation_state(self, img: torch.Tensor):
+        rstate = self.translation_state()
+        return img
+
+    def draw(self, img: torch.Tensor) -> torch.Tensor:
+        img = self._draw_resizing_state(img)
+        img = self._draw_translation_state(img)
+        return img
