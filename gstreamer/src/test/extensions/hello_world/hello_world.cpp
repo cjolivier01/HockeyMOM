89059072781e2416ce3fa81a7207b8705d328059
Chris Olivier <cjolivier01@gmail.comr>
2024-12-09 16:11:55 -0800

    ...

--- gstreamer/src/test/extensions/hello_world/hello_world.cpp
+++ gstreamer/src/test/extensions/hello_world/hello_world.cpp
@@ -4,9 +4,18 @@
 #include "gxf/std/extension_factory_helper.hpp"
 
 GXF_EXT_FACTORY_BEGIN()
-GXF_EXT_FACTORY_SET_INFO(0x05f887afddee3d15, 0xb467168585f324f2, "test",
-                         "A Dummy Example", "", "1.0.0", "NVIDIA");
-GXF_EXT_FACTORY_ADD(0xf849597b07313143, 0xb442234de9857f52, sample::test::HelloWorld,
-                    nvidia::gxf::Codelet, "Dummy example source codelet.");
+GXF_EXT_FACTORY_SET_INFO(
+    0x05f887afddee3d15,
+    0xb467168585f324f3,
+    "hello_world",
+    "colivier:A Dummy Example",
+    "",
+    "1.0.1",
+    "NVIDIA");
+GXF_EXT_FACTORY_ADD(
+    0xf849597b07313143,
+    0xb442234de9857f53,
+    sample::test::HelloWorld,
+    nvidia::gxf::Codelet,
+    "colivier: Dummy example source codelet.");
 GXF_EXT_FACTORY_END()
-  
