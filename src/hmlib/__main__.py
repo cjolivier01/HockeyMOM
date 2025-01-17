0b3670db0dc3e3681a5d18661032b86365739776
Christopher Olivier <cjolivier01@gmail.com>
2025-01-17 04:36:08 -0800

    ...

--- src/hmlib/__main__.py
+++ src/hmlib/__main__.py
@@ -1,3 +1,18 @@
 """
 Main module executor
 """
+
+def main() -> None:
+    print(
+        "Hello and welcome to HockeyMOM!\n"
+        "Available runnable modules are\n"
+        "\n"
+        "hockeymom.hmtrack\n"
+        "hockeymom.stitch\n"
+        "hockeymom.blender2\n"
+        "hockeymom.players\n"
+    )
+    # Add your main script logic here
+
+if __name__ == "__main__":
+    main()
