835160cd360b6e2f207b988ca5933d4b075a20dd
Chris Olivier <cjolivier01@gmail.comr>
2025-01-28 16:51:45 -0800

    ...

--- scripts/video_clipper.py
+++ scripts/video_clipper.py
@@ -37,6 +37,7 @@ ENCODER_ARGS_LOSSLESS = "-c:v hevc_nvenc -preset slow -qp 0 -pix_fmt yuv444p".sp
 # ENCODER_ARGS_FAST = "-c:v hevc_nvenc -preset fast -pix_fmt yuv444p".split(" ")
 # ENCODER_ARGS_FAST = "-c:v hevc_nvenc -preset ultrafast -crf 23 -pix_fmt yuv444p".split(" ")
 ENCODER_ARGS_FAST = "-c:v mpeg4 -preset ultrafast -crf 23".split(" ")
+# ENCODER_ARGS_FAST = "-c:v h264_nvenc -preset p1".split(" ")
 ENCODER_ARGS_HQ = "-c:v hevc_nvenc -preset medium -pix_fmt yuv444p".split(" ")
 
 FFMPEG_CUDA_DECODER = ["-c:v", "hevc_cuvid"]
