fixing the grid sample /masking issues

hmlib/stitching/blender2.py
@@ -159,7 +159,7 @@ class PtImageBlender(torch.jit.ScriptModule):
         assert len(self._unique_values) == 2
         print("Initialized")
 
-    @torch.jit.script_method
+    # @torch.jit.script_method
     def forward(
         self,
         image_1: torch.Tensor,
@@ -668,6 +668,8 @@ class SmartRemapperBlender(torch.nn.Module):
 
         alpha_mask_1 = self._remapper_1.unmapped_mask
         alpha_mask_2 = self._remapper_2.unmapped_mask
+
+        # show_image("alpha_mask_1", alpha_mask_1, wait=False, scale=0.2)
 
         if self._minimize_blend:
             assert image_width(remapped_image_1) == self._remapper_1.width  # sanity

hmlib/stitching/image_remapper.py
@@ -114,7 +114,7 @@ class ImageRemapper(torch.jit.ScriptModule):
             self._dest_w = col_map.shape[1]
             self._dest_h = col_map.shape[0]
 
-            if True:
+            if False:
                 self._working_w = self._dest_w
                 self._working_h = self._dest_h
             else:
@@ -203,7 +203,7 @@ class ImageRemapper(torch.jit.ScriptModule):
             self._remap_op.to(dev)
         return super().to(device, **kwargs)
 
-    @torch.jit.script_method
+    # @torch.jit.script_method
     def forward(self, source_tensor: torch.Tensor) -> torch.Tensor:
         # show_image("mask", self._unmapped_mask, wait=True)
         assert self._initialized
@@ -244,7 +244,7 @@ class ImageRemapper(torch.jit.ScriptModule):
                     align_corners=False,
                 )
                 # destination_tensor = destination_tensor.clamp(min=0, max=255.0).to(torch.uint8)
-        assert self._unmapped_mask.shape[-2] == destination_tensor.shape[-2]
+        assert self._unmapped_mask.shape[-2:] == destination_tensor.shape[-2:]
         destination_tensor[:, :, self._unmapped_mask] = 0
 
         # Add an alpha channel if necessary
@@ -255,5 +255,5 @@ class ImageRemapper(torch.jit.ScriptModule):
         # Clip to the original size that was specified
         assert self._dest_h <= destination_tensor.shape[-2]
         assert self._dest_w <= destination_tensor.shape[-1]
-        destination_tensor = destination_tensor[:, :, : self._dest_h, : self._dest_w]
+        # destination_tensor = destination_tensor[:, :, : self._dest_h, : self._dest_w]
         return destination_tensor

hmlib/stitching/laplacian_blend.py
@@ -3,6 +3,7 @@ from typing import List, Optional, Tuple
 import numpy as np
 import torch
 import torch.nn.functional as F
+from hmlib.ui import show_image
 
 
 def create_gaussian_kernel(
@@ -353,7 +354,7 @@ class LaplacianBlend(torch.jit.ScriptModule):
         for i, m in enumerate(self.mask_small_gaussian_blurred):
             self.mask_small_gaussian_blurred[i] = m.to(*args, **kwargs)
 
-    @torch.jit.script_method
+    # @torch.jit.script_method
     def forward(
         self,
         left: torch.Tensor,
@@ -383,10 +384,24 @@ class LaplacianBlend(torch.jit.ScriptModule):
             canvas_h=canvas_h,
         )
 
+        # show_image("left", left, wait=False)
+
         # For unmapped pixels, put pixels from the other image there, or else
         # it will get interpolated towards black.
-        right[:, :, alpha_mask_right] = left[:, :, alpha_mask_right]
+
+        #show_image("left", left.clone(), wait=False, scale=0.2)
+        # show_image("alpha_mask_left", alpha_mask_left, wait=False, scale=0.2)
+        #show_image("right", right.clone(), wait=False, scale=0.2)
+
+        # show_image("amright", right[:, :, alpha_mask_left], wait=False, scale=0.25)
+
+        # left_tmp = left.clone()
         left[:, :, alpha_mask_left] = right[:, :, alpha_mask_left]
+        right[:, :, alpha_mask_right] = left[:, :, alpha_mask_right]
+        # left = left_tmp
+        # del left_tmp
+
+        # show_image("masked_left", left, wait=False, scale=0.2)
 
         left = to_float(left, dtype=self._dtype)
         right = to_float(right, dtype=self._dtype)