a84edb64afe91be420a55ceb3a8d698966bbc4fe
Chris Olivier <cjolivier01@gmail.comr>
2025-02-09 07:19:13 -0800

    ..

--- format.sh
+++ format.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 # Generate formatted versions as outputs:
-bazel build //... --aspects //tools/format:aspects.bzl%format --output_groups=report
+bazelisk build //... --aspects //tools/format:aspects.bzl%format --output_groups=report
 
 # Overwrite our source files with the formatted outputs:
-bazel run //tools/format:apply.binary -- $@ $(git rev-parse --show-toplevel) $(bazel info bazel-genfiles)
+bazelisk run //tools/format:apply.binary -- $@ $(git rev-parse --show-toplevel) $(bazel info bazel-genfiles)
