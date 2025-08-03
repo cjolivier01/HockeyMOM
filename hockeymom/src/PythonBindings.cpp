1453088626ff9ccf59f5539632a8e610ef720d36
Christopher Olivier <cjolivier01@gmail.com>
2025-08-03 16:09:09 -0700

    ...

--- hockeymom/src/PythonBindings.cpp
+++ hockeymom/src/PythonBindings.cpp
@@ -1305,5 +1305,6 @@ PYBIND11_MODULE(_hockeymom, m) {
   init_box_structures(m);
   init_living_boxes(m);
   init_play_tracker(m);
-  init_cuda_pano<float4>(m);
+  // init_cuda_pano<float4>(m);
+  init_cuda_pano<half4>(m);
 }
