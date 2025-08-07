71f3da92b1d596364894785fada29364bfeaf604
Christopher Olivier <cjolivier01@gmail.com>
2025-08-07 05:34:40 -0700

    ..

--- hmlib/cli/find_ice_rink.py
+++ hmlib/cli/find_ice_rink.py
@@ -8,13 +8,4 @@ from hmlib.segm.ice_rink import main
 from hmlib.utils.gpu import GpuAllocator
 
 if __name__ == "__main__":
-    if "cuda" in args.device and ":" not in args.device:
-        gpu_allocator = GpuAllocator(gpus=args.gpus)
-        device: torch.device = (
-            torch.device("cuda", gpu_allocator.allocate_fast())
-            if not gpu_allocator.is_single_lowmem_gpu(low_threshold_mb=1024 * 10)
-            else torch.device("cpu")
-        )
-    else:
-        device = torch.device(args.device)
-    main(args=None, device=device)
+    main()
