8e1b0a8268a5436d1e7d908f08dd8f80b180af47
Christopher Olivier <cjolivier01@gmail.com>
2025-09-06 10:03:50 -0700

    ..

--- stat.sh
+++ stat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-STAT_FILE="/mnt/ripper-data/Videos/dh-tv-12-1/stats/dh-tv-12-1.xls"
+STAT_FILE="$HOME/Videos/dh-tv-12-1/stats/dh-tv-12-1.xls"
 
 GOALS_FOR="-g GA:1/14:24 -g GA:1/13:49 -g GA:1/12:28 -g GA:1/4:14 -g GA:2/7:17 -g GA:2/3:04 -g GA:3/14:16 -g GA:3/11:35 -g GA:3/11:07"
 GOALS_AGAINST="-g GF:1/02:53 -g GF:2/6:00"
