2dae2f792f704621529267b9d4e5ff1463805563
Christopher Olivier <cjolivier01@gmail.com>
2025-10-10 17:17:07 -0700

    ..

--- hmlib/__init__.py
+++ hmlib/__init__.py
@@ -1,5 +1,21 @@
 import importlib
 
+import numpy as np
+
+# Define a dictionary that mimics the structure of np.sctypes
+# You'll need to populate this with the specific dtypes your code expects
+# This is a simplified example; a full implementation would be more extensive
+_sctypes_mock = {
+    "int": [np.int8, np.int16, np.int32, np.int64],
+    "uint": [np.uint8, np.uint16, np.uint32, np.uint64],
+    "float": [np.float16, np.float32, np.float64],
+    "complex": [np.complex64, np.complex128],
+    "others": [np.bool_, np.object_, np.str_, np.bytes_],
+}
+
+# Monkey-patch np.sctypes
+np.sctypes = _sctypes_mock
+
 from .hm_transforms import HmLoadImageFromWebcam
 
 __all__ = [
