3362dcef1914590a9b2a2559914224c22c2707e3
Christopher Olivier <cjolivier01@gmail.com>
2025-11-16 04:32:05 -0800

    ..

--- hmlib/cli/stitch.py
+++ hmlib/cli/stitch.py
@@ -95,190 +95,196 @@ def stitch_videos(
     post_stitch_rotate_degrees: Optional[float] = None,
     args: Optional[argparse.Namespace] = None,
 ):
-    if configure_only:
-        cache_size = 0
-    if dir_name is None and game_id:
-        dir_name = os.path.join(os.environ["HOME"], "Videos", game_id)
-    left_vid = BasicVideoInfo(",".join(videos["left"]))
-    right_vid = BasicVideoInfo(",".join(videos["right"]))
-    total_frames = min(left_vid.frame_count, right_vid.frame_count)
-    print(f"Total possible stitched video frames: {total_frames}")
+    cuda_stream = torch.cuda.Stream(remapping_device)
+    with torch.cuda.stream(cuda_stream):
+        if configure_only:
+            cache_size = 0
+        if dir_name is None and game_id:
+            dir_name = os.path.join(os.environ["HOME"], "Videos", game_id)
+        left_vid = BasicVideoInfo(",".join(videos["left"]))
+        right_vid = BasicVideoInfo(",".join(videos["right"]))
+        total_frames = min(left_vid.frame_count, right_vid.frame_count)
+        print(f"Total possible stitched video frames: {total_frames}")
 
-    stitch_frame_number = 0
-    if start_frame_time and not stitch_frame_time:
-        stitch_frame_time = start_frame_time
-    if stitch_frame_time or start_frame_time:
-        seconds = convert_hms_to_seconds(stitch_frame_time)
-        if seconds > 0:
-            stitch_frame_number = seconds * left_vid.fps
-    if start_frame_time:
-        assert not start_frame_number
-        seconds = convert_hms_to_seconds(start_frame_time)
-        if seconds > 0:
-            start_frame_number = seconds * left_vid.fps
+        stitch_frame_number = 0
+        if start_frame_time and not stitch_frame_time:
+            stitch_frame_time = start_frame_time
+        if stitch_frame_time or start_frame_time:
+            seconds = convert_hms_to_seconds(stitch_frame_time)
+            if seconds > 0:
+                stitch_frame_number = seconds * left_vid.fps
+        if start_frame_time:
+            assert not start_frame_number
+            seconds = convert_hms_to_seconds(start_frame_time)
+            if seconds > 0:
+                start_frame_number = seconds * left_vid.fps
 
-    pto_project_file, lfo, rfo = configure_video_stitching(
-        dir_name,
-        video_left=str(videos["left"][0]),
-        video_right=str(videos["right"][0]),
-        project_file_name=project_file_name,
-        left_frame_offset=lfo,
-        right_frame_offset=rfo,
-        base_frame_offset=stitch_frame_number,
-        max_control_points=max_control_points,
-        force=force,
-    )
-
-    stitch_videos = {
-        "left": {
-            "files": videos["left"],
-            "frame_offset": lfo,
-        },
-        "right": {
-            "files": videos["right"],
-            "frame_offset": rfo,
-        },
-    }
-
-    data_loader = StitchDataset(
-        pto_project_file=pto_project_file,
-        videos=stitch_videos,
-        start_frame_number=start_frame_number,
-        max_frames=max_frames,
-        batch_size=batch_size,
-        num_workers=1,
-        max_input_queue_size=cache_size,
-        image_roi=(get_clip_box(game_id=game_id, root_dir=ROOT_DIR) if not ignore_clip_box else None),
-        decoder_device=decoder_device,
-        blend_mode=blend_mode,
-        remapping_device=remapping_device,
-        dtype=dtype,
-        auto_adjust_exposure=auto_adjust_exposure,
-        minimize_blend=minimize_blend,
-        python_blender=python_blender,
-        post_stitch_rotate_degrees=post_stitch_rotate_degrees,
-    )
-
-    data_loader_iter = CachedIterator(iterator=iter(data_loader), cache_size=cache_size)
-
-    frame_count = 0
-    dataset_delivery_fps = 0.0
-
-    use_progress_bar: bool = True
-    scroll_output: Optional[ScrollOutput] = None
-
-    shower = None
-    if show:
-        shower = Shower(
-            label="stitched_image",
-            show_scaled=show_scaled,
-            cache_on_cpu=lowmem,
+        pto_project_file, lfo, rfo = configure_video_stitching(
+            dir_name,
+            video_left=str(videos["left"][0]),
+            video_right=str(videos["right"][0]),
+            project_file_name=project_file_name,
+            left_frame_offset=lfo,
+            right_frame_offset=rfo,
+            base_frame_offset=stitch_frame_number,
+            max_control_points=max_control_points,
+            force=force,
         )
 
-    if use_progress_bar and not configure_only:
-        total_frame_count = len(data_loader)
+        stitch_videos = {
+            "left": {
+                "files": videos["left"],
+                "frame_offset": lfo,
+            },
+            "right": {
+                "files": videos["right"],
+                "frame_offset": rfo,
+            },
+        }
 
-        def _table_callback(table_map: OrderedDict):
-            table_map["Stitching Dataset Delivery FPS"] = "{:.2f}".format(dataset_delivery_fps)
-            if dataset_delivery_fps > 0:
-                remaining_secs = (total_frame_count - frame_count) / dataset_delivery_fps
-                table_map["Time Remaining"] = convert_seconds_to_hms(remaining_secs)
-
-        scroll_output = ScrollOutput()
-
-        scroll_output.register_logger(logger)
-
-        progress_bar = ProgressBar(
-            total=total_frame_count,
-            iterator=data_loader_iter,
-            scroll_output=scroll_output,
-            update_rate=20,
-            table_callback=_table_callback,
-            use_curses=True,
-            enable_gpu_metrics=getattr(args, "progress_gpu_metrics", True) if args is not None else True,
-            enable_cuda_sync_counter=getattr(args, "progress_cuda_sync_counter", True) if args is not None else True,
+        data_loader = StitchDataset(
+            pto_project_file=pto_project_file,
+            videos=stitch_videos,
+            start_frame_number=start_frame_number,
+            max_frames=max_frames,
+            batch_size=batch_size,
+            num_workers=1,
+            max_input_queue_size=cache_size,
+            image_roi=(get_clip_box(game_id=game_id, root_dir=ROOT_DIR) if not ignore_clip_box else None),
+            decoder_device=decoder_device,
+            blend_mode=blend_mode,
+            remapping_device=remapping_device,
+            dtype=dtype,
+            auto_adjust_exposure=auto_adjust_exposure,
+            minimize_blend=minimize_blend,
+            python_blender=python_blender,
+            post_stitch_rotate_degrees=post_stitch_rotate_degrees,
         )
-        data_loader_iter = progress_bar
 
-    video_out = None
+        data_loader_iter = CachedIterator(iterator=iter(data_loader), cache_size=cache_size)
 
-    def _maybe_save_frame(frame: torch.Tensor) -> None:
-        nonlocal video_out
-        if output_stitched_video_file and video_out is None:
-            video_out = VideoOutput(
-                args=None,
-                output_video_path=output_stitched_video_file,
-                output_frame_width=image_width(frame),
-                output_frame_height=image_height(frame),
-                fps=data_loader.fps,
-                video_out_pipeline=None,
-                max_queue_backlog=cache_size,
-                device=encoder_device,
-                simple_save=True,
-                skip_final_save=False,
-                original_clip_box=None,
-                progress_bar=progress_bar,
-                cache_size=cache_size,
-                async_output=async_output,
+        frame_count = 0
+        dataset_delivery_fps = 0.0
+
+        use_progress_bar: bool = True
+        scroll_output: Optional[ScrollOutput] = None
+
+        shower = None
+        if show:
+            shower = Shower(
+                label="stitched_image",
+                show_scaled=show_scaled,
+                cache_on_cpu=lowmem,
             )
-        if video_out is not None:
-            video_out.append(dict(img=frame))
 
-    try:
-        start = None
+        if use_progress_bar and not configure_only:
+            total_frame_count = len(data_loader)
 
-        dataset_timer = Timer()
-        for i, stitched_image in enumerate(data_loader_iter):
-            if configure_only:
-                break
-            if not output_stitched_video_file and isinstance(stitched_image, StreamTensor):
-                stitched_image._verbose = False
-                stitched_image = stitched_image.get()
+            def _table_callback(table_map: OrderedDict):
+                table_map["Stitching Dataset Delivery FPS"] = "{:.2f}".format(dataset_delivery_fps)
+                if dataset_delivery_fps > 0:
+                    remaining_secs = (total_frame_count - frame_count) / dataset_delivery_fps
+                    table_map["Time Remaining"] = convert_seconds_to_hms(remaining_secs)
 
-            _maybe_save_frame(frame=stitched_image)
+            scroll_output = ScrollOutput()
 
-            if shower is not None:
-                if False and stitched_image.device.type == "cuda":
-                    for stitched_img in stitched_image:
-                        show_cuda_tensor(
-                            "Stitched Image", stitched_img.clamp(min=0, max=255).to(torch.uint8), False, None
+            scroll_output.register_logger(logger)
+
+            progress_bar = ProgressBar(
+                total=total_frame_count,
+                iterator=data_loader_iter,
+                scroll_output=scroll_output,
+                update_rate=20,
+                table_callback=_table_callback,
+                use_curses=True,
+                enable_gpu_metrics=getattr(args, "progress_gpu_metrics", True) if args is not None else True,
+                enable_cuda_sync_counter=(
+                    getattr(args, "progress_cuda_sync_counter", True) if args is not None else True
+                ),
+            )
+            data_loader_iter = progress_bar
+
+        video_out = None
+
+        def _maybe_save_frame(frame: torch.Tensor) -> None:
+            nonlocal video_out
+            if output_stitched_video_file and video_out is None:
+                video_out = VideoOutput(
+                    args=None,
+                    output_video_path=output_stitched_video_file,
+                    output_frame_width=image_width(frame),
+                    output_frame_height=image_height(frame),
+                    fps=data_loader.fps,
+                    video_out_pipeline=None,
+                    max_queue_backlog=cache_size,
+                    device=encoder_device,
+                    simple_save=True,
+                    skip_final_save=False,
+                    original_clip_box=None,
+                    progress_bar=progress_bar,
+                    cache_size=cache_size,
+                    async_output=async_output,
+                )
+            if video_out is not None:
+                video_out.append(dict(img=frame))
+
+        try:
+            start = None
+
+            dataset_timer = Timer()
+            for i, stitched_image in enumerate(data_loader_iter):
+                if configure_only:
+                    break
+                if not output_stitched_video_file and isinstance(stitched_image, StreamTensor):
+                    stitched_image._verbose = False
+                    stitched_image = stitched_image.get()
+
+                _maybe_save_frame(frame=stitched_image)
+
+                if shower is not None:
+                    if False and stitched_image.device.type == "cuda":
+                        for stitched_img in stitched_image:
+                            show_cuda_tensor(
+                                "Stitched Image", stitched_img.clamp(min=0, max=255).to(torch.uint8), False, None
+                            )
+                    else:
+                        shower.show(stitched_image)
+
+                cuda_stream.synchronize()
+
+                if i > 1:
+                    dataset_timer.toc()
+                if (i + 1) % 20 == 0:
+                    assert stitched_image.ndim == 4
+                    dataset_delivery_fps = batch_size / max(1e-5, dataset_timer.average_time)
+                    logger.info(
+                        "Dataset frame {} ({:.2f} fps)".format(
+                            i * batch_size,
+                            batch_size / max(1e-5, dataset_timer.average_time),
                         )
-                else:
-                    shower.show(stitched_image)
+                    )
+                    if i % 100 == 0:
+                        dataset_timer = Timer()
 
-            if i > 1:
-                dataset_timer.toc()
-            # if (i + 1) % 20 == 0:
-            #     assert stitched_image.ndim == 4
-            #     dataset_delivery_fps = batch_size / max(1e-5, dataset_timer.average_time)
-            #     logger.info(
-            #         "Dataset frame {} ({:.2f} fps)".format(
-            #             i * batch_size,
-            #             batch_size / max(1e-5, dataset_timer.average_time),
-            #         )
-            #     )
-            #     if i % 100 == 0:
-            #         dataset_timer = Timer()
+                frame_count += batch_size
 
-            frame_count += batch_size
+                if i == 1:
+                    start = time.time()
+                dataset_timer.tic()
 
-            if i == 1:
-                start = time.time()
-            dataset_timer.tic()
+                del stitched_image
 
-            del stitched_image
-
-        if start is not None:
-            duration = time.time() - start
-            print(f"{frame_count} frames in {duration} seconds ({(frame_count)/duration} fps)")
-    except StopIteration:
-        pass
-    finally:
-        data_loader.close()
-        if shower is not None:
-            shower.close()
-        if video_out is not None:
-            video_out.stop()
+            if start is not None:
+                duration = time.time() - start
+                print(f"{frame_count} frames in {duration} seconds ({(frame_count)/duration} fps)")
+        except StopIteration:
+            pass
+        finally:
+            data_loader.close()
+            if shower is not None:
+                shower.close()
+            if video_out is not None:
+                video_out.stop()
     return lfo, rfo
 
 
