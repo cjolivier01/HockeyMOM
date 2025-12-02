71562db49931e2e32563b7d4ebe2b990dcd9f711
Chris Olivier <cjolivier01@apache.org>
2025-12-02 14:02:12 -0800

    ..

--- tests/test_tracker_trunk.py
+++ tests/test_tracker_trunk.py
@@ -2,12 +2,12 @@ import unittest
 
 import torch
 
-from hmlib.aspen.trunks.tracker import TrackerTrunk
+from hmlib.aspen.plugins.tracker_plugin import TrackerPlugin
 
 
-class TrackerTrunkConfigTest(unittest.TestCase):
+class TrackerPluginConfigTest(unittest.TestCase):
     def test_default_tracker_class_instantiates_cpu_tracker(self):
-        trunk = TrackerTrunk(enabled=True)
+        trunk = TrackerPlugin(enabled=True)
         trunk._ensure_tracker(image_size=torch.Size([1, 3, 720, 1280]))
         self.assertIsNotNone(trunk._hm_tracker)
         self.assertEqual(trunk.tracker_class, "hockeymom.core.HmTracker")
@@ -15,7 +15,7 @@ class TrackerTrunkConfigTest(unittest.TestCase):
 
     @unittest.skipUnless(torch.cuda.is_available(), "CUDA not available")
     def test_cuda_tracker_class_instantiates_cuda_tracker(self):
-        trunk = TrackerTrunk(
+        trunk = TrackerPlugin(
             enabled=True,
             tracker_class="hockeymom.core.HmByteTrackerCuda",
             tracker_kwargs={"device": "cuda:0"},
@@ -26,7 +26,7 @@ class TrackerTrunkConfigTest(unittest.TestCase):
         self.assertEqual(trunk._hm_tracker.__class__.__name__, "HmByteTrackerCuda")
 
     def test_prepare_tracker_inputs_with_static_limits(self):
-        trunk = TrackerTrunk(enabled=True)
+        trunk = TrackerPlugin(enabled=True)
         trunk._static_tracker_max_detections = 4
         trunk._static_tracker_max_tracks = 8
         bboxes = torch.arange(20, dtype=torch.float32).reshape(5, 4)
