1647bcf748768b34a58ab279371552908a0c83e4
Chris Olivier <cjolivier01@apache.org>
2026-01-03 12:25:37 -0800

    ..

--- scripts/install_codecs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# For FFMPeg H265
-sudo apt-get install -y libx264-dev libx265-dev libvpx-dev libfdk-aac-dev libmp3lame-dev libopus-dev
-
--- scripts/unblur.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ffmpeg -hwaccel cuda -i "$1" -vf "unsharp=3:3:1.5:3:3:0.5" -c:v hevc_nvenc -preset lossless -acodec copy $2
- 
