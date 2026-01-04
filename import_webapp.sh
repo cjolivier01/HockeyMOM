9cf2af8ed0ed858325e00369163f29013db3ec0c
Chris Olivier <cjolivier01@apache.org>
2026-01-03 18:58:28 -0800

    ok

--- import_webapp.sh
+++ import_webapp.sh
@@ -4,5 +4,5 @@
 # ./p tools/webapp/import_time2score.py --source=caha --league-name=Norcal --season 0 --config /opt/hm-webapp/app/config.json --user-email cjolivier01@gmail.com
 # ./p tools/webapp/import_time2score.py --source=caha --league-name=Norcal --season 0 --config /opt/hm-webapp/app/config.json --user-email cjolivier01@gmail.com --division 6:0
 ./p tools/webapp/import_time2score.py --source=caha --league-name=Norcal --season 0 --api-url http://127.0.0.1:8008 --user-email cjolivier01@gmail.com --division 6:0
-./p scripts/parse_shift_spreadsheet.py --file-list ~/Videos/game_list_long.txt --shifts --upload-webapp --webapp-url=http://127.0.0.1:8008 --webapp-owner-email cjolivier01@gmail.com --league-name=Norcal
+./p scripts/parse_shift_spreadsheet.py --file-list ~/Videos/game_list_long.txt --shifts --upload-webapp --webapp-url=http://127.0.0.1:8008 --webapp-owner-email cjolivier01@gmail.com --webapp-league-name=Norcal
 ./p tools/webapp/dedupe_league_teams.py --config /opt/hm-webapp/app/config.json --league-name Norcal --yes
