4ea330a4f8a637fa7ccec61e6592c29288d13f1f
Chris Olivier <cjolivier01@apache.org>
2026-01-25 11:33:43 -0800

    ...

--- doall_2.sh
+++ doall_2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in $(cat ~/RVideos/all2.lst); do echo $i; ./hm_run.sh --game-id=$i $@; done
+for i in $(cat ~/RVideos/all2.lst | grep -v '#' ); do echo $i; ./hm_run.sh --game-id=$i $@; done
