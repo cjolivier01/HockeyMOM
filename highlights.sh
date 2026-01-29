41ef56ca54702a99f8d62bb53310c08942a09ac3
Chris Olivier <cjolivier01@apache.org>
2026-01-29 15:14:14 -0800

    Added highl;ights.sh

---
+++ highlights.sh
@@ -0,0 +1,3 @@
+./p scripts/parse_stats_inputs.py --season-highlight-types Goal --season-highlight-window-seconds=10 --clip-transition-seconds --file-list $HOME/Videos/jrsharks2013-2025-2026/game_list_long.yaml $@
+
+
