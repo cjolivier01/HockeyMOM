1c84fb0202d70844b0e48cc5a9accabb5221102a
Chris Olivier <cjolivier01@apache.org>
2026-02-08 12:04:31 -0800

    ..

--- train_gpt.sh
+++ train_gpt.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./camgpt_train.sh --file list ~/RVideos/all.lst --no-pose --include-rink --steps 300000 --data-workers 32 --preload-csv shard --max-checkpoints 10 $@
+./camgpt_train.sh --file-list ~/RVideos/all.lst --no-pose --include-rink --steps 300000 --data-workers 32 --preload-csv shard --max-checkpoints 10 $@
