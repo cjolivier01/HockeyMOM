0149437a6ed062cef7cba8c21147f2c7c47b2735
Chris Olivier <cjolivier01@apache.org>
2026-03-28 05:08:20 -0700

    pythonpath

--- scripts/scoreboard_select.py
+++ scripts/scoreboard_select.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), "..")))
+
 from hmlib.hm_opts import hm_opts
 from hmlib.scoreboard.selector import configure_scoreboard, get_max_screen_height
 
