9d9d917e6d1f339f24ca05bf4889307c25694993
Chris Olivier <cjolivier01@apache.org>
2026-04-25 04:21:41 -0700

    Remove torchvision video decode backend and clean decoder CLI options (#105)
    * fix(video): remove torchvision video decode backend
    
    * refactor(video): keep only pynvcodec decoder name

--- hmlib/hm_opts.py
+++ hmlib/hm_opts.py
@@ -1237,7 +1237,7 @@ class hm_opts(object):
             default="auto",
             type=str,
             help=(
-                "Video stream decode method [auto, cv2, ffmpeg, torchvision, torchaudio, "
+                "Video stream decode method [auto, cv2, ffmpeg, torchaudio, "
                 "gstreamer, pynvcodec]"
             ),
         )
@@ -1245,7 +1245,7 @@ class hm_opts(object):
             "--decoder-device",
             default="cuda",
             type=str,
-            help="Video stream decode method [cv2, ffmpeg, torchvision, tochaudio]",
+            help="Video stream decode method [cv2, ffmpeg, torchaudio, gstreamer, pynvcodec]",
         )
         parser.add_argument(
             "--encoder-device",
@@ -1270,7 +1270,7 @@ class hm_opts(object):
         #     dest="video_stream_encode_method",
         #     default="cv2",
         #     type=str,
-        #     help="Video stream decode method [cv2, ffmpeg, torchvision, tochaudio]",
+        #     help="Video stream decode method [cv2, ffmpeg, torchaudio, gstreamer, pynvcodec]",
         # )
         parser.add_argument(
             "-o",
